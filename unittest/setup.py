e71f9f41c43f1ceb8829a72dffada81ecd06ed7f
Paul Sokolovsky <pfalcon@users.sourceforge.net>
2014-04-02 00:20:23 +0300

    unittest: Add packaging metadata file (setup.py).

---
+++ unittest/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(name='micropython-unittest',
+      version='0.0.1',
+      description='Reimplementation of unittest package for MicroPython',
+      url='https://github.com/micropython/micropython/issues/405',
+      author='MicroPython Developers',
+      author_email='micro-python@googlegroups.com',
+      license='MIT',
+      py_modules=['unittest'])
