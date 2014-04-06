122d1781e3a919d5954d90683df411d0bceb2aea
Paul Sokolovsky <pfalcon@users.sourceforge.net>
2014-04-06 23:30:48 +0300

    copy: Switch to setuptools to support dependencies (types module).

--- copy/setup.py
+++ copy/setup.py
@@ -2,14 +2,15 @@ import sys
 # Remove current dir from sys.path, otherwise distutils will peek up our
 # copy module instead of system.
 sys.path.pop(0)
-from distutils.core import setup
+from setuptools import setup
 
 setup(name='micropython-copy',
-      version='0.0.1',
+      version='0.0.2',
       description='CPython copy module ported to MicroPython',
       url='https://github.com/micropython/micropython/issues/405',
       author='CPython Developers',
       maintainer='MicroPython Developers',
       maintainer_email='micro-python@googlegroups.com',
       license='Python',
+      install_requires=['micropython-types'],
       py_modules=['copy'])
