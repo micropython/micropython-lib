5cd6cf494c67025bde8aca90a47e3d4725904b73
Paul Sokolovsky <pfalcon@users.sourceforge.net>
2014-04-06 23:02:16 +0300

    copy: Add setup metadata.

---
+++ copy/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(name='micropython-copy',
+      version='0.0.1',
+      description='CPython copy module ported to MicroPython',
+      url='https://github.com/micropython/micropython/issues/405',
+      author='CPython Developers',
+      maintainer='MicroPython Developers',
+      maintainer_email='micro-python@googlegroups.com',
+      license='Python',
+      py_modules=['copy'])
