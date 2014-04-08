833efb514446b45066f4e2d9c29e7c3c18a1c578
Paul Sokolovsky <pfalcon@users.sourceforge.net>
2014-04-08 19:32:42 +0300

    re-pcre: Add setup metadata.

---
+++ re-pcre/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(name='micropython-re-pcre',
+      version='0.0.1',
+      description='re module for MicroPython, based on PCRE and FFI',
+      url='https://github.com/micropython/micropython/issues/405',
+      author='Paul Sokolovsky',
+      author_email='micro-python@googlegroups.com',
+      license='MIT',
+      py_modules=['re'])
