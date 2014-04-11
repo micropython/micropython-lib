1bc138fcf33b1c8a1d82ba33099d237f919ef2df
Paul Sokolovsky <pfalcon@users.sourceforge.net>
2014-04-11 21:18:49 +0300

    re-pcre: Bump version to 0.1.
    Implements enough functionality to run json module.

--- re-pcre/setup.py
+++ re-pcre/setup.py
@@ -1,7 +1,7 @@
 from distutils.core import setup
 
 setup(name='micropython-re-pcre',
-      version='0.0.2',
+      version='0.1',
       description='re module for MicroPython, based on PCRE and FFI',
       url='https://github.com/micropython/micropython/issues/405',
       author='Paul Sokolovsky',
