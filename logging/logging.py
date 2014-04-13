86d676cf58cd5f4d49f04adf00c54f7c45caa51b
Paul Sokolovsky <pfalcon@users.sourceforge.net>
2014-04-13 04:57:45 +0300

    logging: Add dummy module to preclude import errors.

---
+++ logging/logging.py
@@ -0,0 +1,5 @@
+# Dummy file to preclude import errors
+# Should be reimplemented for MicroPython
+# Reason:
+# Basic, useful module, by CPython impl depends on module "string" which
+# uses metaclasses.
