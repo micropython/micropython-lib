3def1345fa7bab25c73e70ee52744c8be6b140a7
Paul Sokolovsky <pfalcon@users.sourceforge.net>
2014-04-17 01:29:02 +0300

    warnings: Add setup metadata.

---
+++ warnings/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(name='micropython-warnings',
+      version='0.0.1',
+      description='Dummy warnings package for MicroPython',
+      url='https://github.com/micropython/micropython/issues/405',
+      author='MicroPython Developers',
+      author_email='micro-python@googlegroups.com',
+      license='MIT',
+      py_modules=['warnings'])
