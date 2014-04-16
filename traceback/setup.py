966b6c1b808bac336d8d4f8db64cafb40547e9ed
Paul Sokolovsky <pfalcon@users.sourceforge.net>
2014-04-17 01:31:27 +0300

    traceback: Add setup metadata.

---
+++ traceback/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(name='micropython-traceback',
+      version='0.0.1',
+      description='Dummay traceback package for MicroPython',
+      url='https://github.com/micropython/micropython/issues/405',
+      author='MicroPython Developers',
+      author_email='micro-python@googlegroups.com',
+      license='MIT',
+      py_modules=['traceback'])
