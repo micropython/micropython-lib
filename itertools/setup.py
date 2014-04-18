fc01d606ba3d69a51230f851247d51fe45b36af5
Paul Sokolovsky <pfalcon@users.sourceforge.net>
2014-04-18 04:32:11 +0300

    itertools: Add setup metadata.

---
+++ itertools/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(name='micropython-itertools',
+      version='0.1',
+      description='itertools module for MicroPython',
+      url='https://github.com/micropython/micropython/issues/405',
+      author='Paul Sokolovsky',
+      author_email='micro-python@googlegroups.com',
+      license='MIT',
+      py_modules=['itertools'])
