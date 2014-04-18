607c28092bd6750ad810ce481cca4fa5fc9ce724
Paul Sokolovsky <pfalcon@users.sourceforge.net>
2014-04-18 03:06:20 +0300

    collections.deque: Add initial implementation.

---
+++ collections.deque/collections/deque.py
@@ -0,0 +1,22 @@
+class deque:
+
+    def __init__(self, iterable=None):
+        if iterable is None:
+            self.q = []
+        else:
+            self.q = list(iterable)
+
+    def popleft(self):
+        return self.q.pop(0)
+
+    def popright(self):
+        return self.q.pop()
+
+    def pop(self):
+        return self.q.pop()
+
+    def append(self, a):
+        self.q.append(a)
+
+    def appendleft(self, a):
+        self.q = [a] + self.q
