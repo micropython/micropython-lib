effbf07c9e967f4fdbc7fce241178496b5a10a8a
Paul Sokolovsky <pfalcon@users.sourceforge.net>
2014-04-19 20:30:31 +0300

    os: Version bump.

--- os/setup.py
+++ os/setup.py
@@ -1,7 +1,7 @@
 from distutils.core import setup
 
 setup(name='micropython-os',
-      version='0.0.1',
+      version='0.0.2',
       description='os module for MicroPython',
       url='https://github.com/micropython/micropython/issues/405',
       author='MicroPython Developers',
