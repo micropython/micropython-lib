6cac26f078434d89e06784a0fecca5b70a0331d9
Paul Sokolovsky <pfalcon@users.sourceforge.net>
2014-04-19 21:01:44 +0300

    select: Add setup metadata.

---
+++ select/setup.py
@@ -0,0 +1,15 @@
+import sys
+# Remove current dir from sys.path, otherwise distutils will peek up our
+# copy module instead of system.
+sys.path.pop(0)
+from setuptools import setup
+
+setup(name='micropython-select',
+      version='0.0.1',
+      description='select module to MicroPython',
+      url='https://github.com/micropython/micropython/issues/405',
+      author='Paul Sokolovsky',
+      author_email='micro-python@googlegroups.com',
+      license='MIT',
+      install_requires=['micropython-os'],
+      py_modules=['select'])
