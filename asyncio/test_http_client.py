f0ce7978cec1ef5e8022348633001cd2fb13b8ff
Paul Sokolovsky <pfalcon@users.sourceforge.net>
2014-04-20 06:27:43 +0300

    asyncio: Add basic asyncio stream interface test.

---
+++ asyncio/test_http_client.py
@@ -0,0 +1,25 @@
+import asyncio
+
+@asyncio.coroutine
+def print_http_headers(url):
+    reader, writer = yield from asyncio.open_connection(url, 80)
+    print(reader, writer)
+    print("================")
+    query = "GET / HTTP/1.0\r\n\r\n"
+    yield from writer.write(query.encode('latin-1'))
+    while True:
+        line = yield from reader.readline()
+        if not line:
+            break
+        if line:
+            print(line.rstrip())
+
+import logging
+logging.basicConfig(level=logging.INFO)
+url = "google.com"
+loop = asyncio.get_event_loop()
+#task = asyncio.async(print_http_headers(url))
+#loop.run_until_complete(task)
+loop.call_soon(print_http_headers(url))
+loop.run_forever()
+loop.close()
