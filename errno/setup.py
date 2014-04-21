1d69a4a0d540a344b1e93b339a4a7c751f2960ac
Paul Sokolovsky <pfalcon@users.sourceforge.net>
2014-04-22 00:28:58 +0300

    errno: Add setup metadata.

---
+++ errno/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(name='micropython-errno',
+      version='0.1',
+      description='errno module for MicroPython',
+      url='https://github.com/micropython/micropython/issues/405',
+      author='MicroPython Developers',
+      author_email='micro-python@googlegroups.com',
+      license='MIT',
+      py_modules=['errno'])
