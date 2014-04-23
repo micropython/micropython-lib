fa273cae71638fa4bf2c7d02a23b6509b1c8b00e
Paul Sokolovsky <pfalcon@users.sourceforge.net>
2014-04-24 01:35:10 +0300

    asyncio: Add basic loop.call_soon() test.

---
+++ asyncio/test_call_soon.py
@@ -0,0 +1,13 @@
+import asyncio
+import time
+
+
+def cb():
+    print("callback")
+    time.sleep(0.5)
+    loop.call_soon(cb)
+
+
+loop = asyncio.get_event_loop()
+loop.call_soon(cb)
+loop.run_forever()
