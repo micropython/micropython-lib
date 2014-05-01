b975f8652caaac8868bc8a965fee1cfe1e937735
Paul Sokolovsky <pfalcon@users.sourceforge.net>
2014-05-01 10:45:17 +0300

    multiprocessing: Update metadata.

--- multiprocessing/setup.py
+++ multiprocessing/setup.py
@@ -5,11 +5,11 @@ sys.path.pop(0)
 from setuptools import setup
 
 setup(name='micropython-multiprocessing',
-      version='0.0.1',
+      version='0.0.2',
       description='multiprocessing module to MicroPython',
       url='https://github.com/micropython/micropython/issues/405',
       author='Paul Sokolovsky',
       author_email='micro-python@googlegroups.com',
       license='MIT',
-      install_requires=['micropython-os'],
+      install_requires=['micropython-os', 'micropython-select', 'micropython-pickle'],
       py_modules=['multiprocessing'])
