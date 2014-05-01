c2f094e29ba2b953080f7544aabcc6061e99ba35
Paul Sokolovsky <pfalcon@users.sourceforge.net>
2014-05-01 10:44:22 +0300

    multiprocessing: Add true async proof-test.

--- multiprocessing/test_pool_async.py
+++ multiprocessing/test_pool_async.py
@@ -15,6 +15,27 @@ def f2(x):
 future = pool.apply_async(f2, (10,))
 while not future.ready():
     print("not ready")
-    time.sleep(0.1)
+    time.sleep(0.2)
 
 print(future.get())
+
+
+t = time.time()
+futs = [
+    pool.apply_async(f2, (10,)),
+    pool.apply_async(f2, (11,)),
+    pool.apply_async(f2, (12,)),
+]
+
+while True:
+    #not all(futs):
+    c = 0
+    for f in futs:
+        if not f.ready():
+            c += 1
+    if not c:
+        break
+    print("not ready2")
+    time.sleep(0.2)
+
+print("Run 3 parallel sleep(1)'s in: ", time.time() - t)
