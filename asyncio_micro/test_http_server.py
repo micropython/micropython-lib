fe23bdfa18f5c1e58845ee21b04a261fee53b6d8
Paul Sokolovsky <pfalcon@users.sourceforge.net>
2014-05-07 02:14:28 +0300

    asyncio_micro: Add basic HTTP server example.

---
+++ asyncio_micro/test_http_server.py
@@ -0,0 +1,21 @@
+import asyncio_micro as asyncio
+
+@asyncio.coroutine
+def serve(reader, writer):
+    print(reader, writer)
+    print("================")
+    print((yield from reader.read()))
+    yield from writer.write("HTTP/1.0 200 OK\r\n\r\nHello.\r\n")
+    print("After response write")
+    yield from writer.close()
+    print("Finished processing request")
+
+
+import logging
+#logging.basicConfig(level=logging.INFO)
+logging.basicConfig(level=logging.DEBUG)
+loop = asyncio.get_event_loop()
+mem_info()
+loop.call_soon(asyncio.start_server(serve, "127.0.0.1", 8081))
+loop.run_forever()
+loop.close()
