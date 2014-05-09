9f7baad5eee4ff6bae85673500fcb5cc8c14c6a0
Paul Sokolovsky <pfalcon@users.sourceforge.net>
2014-05-09 03:43:58 +0300

    binascii: Add dummy module.

---
+++ binascii/setup.py
@@ -0,0 +1,18 @@
+import sys
+# Remove current dir from sys.path, otherwise distutils will peek up our
+# module instead of system one.
+sys.path.pop(0)
+sys.path.insert(0, '..')
+from setuptools import setup
+import metadata
+
+NAME = 'binascii'
+
+setup(name='micropython-' + NAME,
+      version='0.0.0',
+      description=metadata.desc_dummy(NAME),
+      url=metadata.url,
+      author=metadata.author_upy_devels,
+      author_email=metadata.author_upy_devels_email,
+      license='MIT',
+      py_modules=[NAME])
