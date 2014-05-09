3df885dfa702bfa47f940dabb555b9862f015ddd
Paul Sokolovsky <pfalcon@users.sourceforge.net>
2014-05-10 00:42:42 +0300

    fnmatch: Update metadata.

--- fnmatch/setup.py
+++ fnmatch/setup.py
@@ -1,18 +1,23 @@
 import sys
 # Remove current dir from sys.path, otherwise distutils will peek up our
-# module instead of system one.
+# module instead of system.
 sys.path.pop(0)
-sys.path.insert(0, '..')
 from setuptools import setup
-import metadata
+
+
+def desc_dummy(name):
+    return 'Dummy %s module to MicroPython' % name
+def desc_cpython(name):
+    return 'CPython %s module ported to MicroPython' % name
 
 NAME = 'fnmatch'
 
 setup(name='micropython-' + NAME,
-      version='0.0.0',
-      description=metadata.desc_dummy(NAME),
-      url=metadata.url,
-      author=metadata.author_upy_devels,
-      author_email=metadata.author_upy_devels_email,
-      license='MIT',
+      version='0.5',
+      description=desc_cpython(NAME),
+      url='https://github.com/micropython/micropython/issues/405',
+      author='CPython Developers',
+      maintainer='MicroPython Developers',
+      maintainer_email='micro-python@googlegroups.com',
+      license='Python',
       py_modules=[NAME])
