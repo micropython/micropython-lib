6074bd8e43b6897fdb52057e2eecfaf589971c17
Paul Sokolovsky <pfalcon@users.sourceforge.net>
2014-05-09 23:34:23 +0300

    os.path: Start implementing module.

---
+++ os.path/os/path.py
@@ -0,0 +1,2 @@
+def normcase(s):
+    return s
---
+++ os.path/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(name='micropython-os.path',
+      version='0.0.1',
+      description='os.path module for MicroPython',
+      url='https://github.com/micropython/micropython/issues/405',
+      author='MicroPython Developers',
+      author_email='micro-python@googlegroups.com',
+      license='MIT',
+      packages=['os'])
