87724fadda22724462eb1a1cf1750e175a800dc8
Paul Sokolovsky <pfalcon@users.sourceforge.net>
2014-05-09 23:24:02 +0300

    os: Convert to package, require to support os.path.

--- os/setup.py
+++ os/setup.py
@@ -1,10 +1,10 @@
 from distutils.core import setup
 
 setup(name='micropython-os',
-      version='0.0.4',
+      version='0.0.5',
       description='os module for MicroPython',
       url='https://github.com/micropython/micropython/issues/405',
       author='MicroPython Developers',
       author_email='micro-python@googlegroups.com',
       license='MIT',
-      py_modules=['os'])
+      packages=['os'])
