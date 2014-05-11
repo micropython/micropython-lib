35d4ae900bdfcccd87140c3655b17b2748711958
Paul Sokolovsky <pfalcon@users.sourceforge.net>
2014-05-11 21:30:44 +0300

    unittest: Bump version.

--- unittest/setup.py
+++ unittest/setup.py
@@ -1,7 +1,7 @@
 from distutils.core import setup
 
 setup(name='micropython-unittest',
-      version='0.0.3',
+      version='0.0.4',
       description='Reimplementation of unittest package for MicroPython',
       url='https://github.com/micropython/micropython/issues/405',
       author='MicroPython Developers',
