5c70cace45f4ceb6349d6c0c8188880925a4713d
Paul Sokolovsky <pfalcon@users.sourceforge.net>
2014-05-11 21:25:22 +0300

    unittest: assertEqual(): make a hint which value is expected.

--- unittest/unittest.py
+++ unittest/unittest.py
@@ -4,7 +4,7 @@ class TestCase:
         assert False, msg
 
     def assertEqual(self, x, y, msg=''):
-        assert x == y, "%r vs %r" % (x, y)
+        assert x == y, "%r vs (expected) %r" % (x, y)
 
     def assertTrue(self, x, msg=''):
         assert x, msg
