112625edaf897ebc82bd1114cb8b458a9d9d6235
Paul Sokolovsky <pfalcon@users.sourceforge.net>
2014-05-12 12:59:43 +0300

    Remove failed attempt of having centralized metadata.

--- metadata.py
+++
@@ -1,7 +0,0 @@
-url='https://github.com/micropython/micropython/issues/405'
-author_upy_devels = 'MicroPython Developers'
-author_upy_devels_email = 'micro-python@googlegroups.com'
-
-# Dummy module
-def desc_dummy(name):
-    return 'Dummy %s module to MicroPython' % name
