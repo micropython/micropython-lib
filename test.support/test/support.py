31f20a083f85cb7c17e822de7c6c4ab4708b39c8
Paul Sokolovsky <pfalcon@users.sourceforge.net>
2014-05-14 21:51:02 +0300

    test.support: Addmore functions.
    can_symlink() so far returns False for simplicity.

--- test.support/test/support.py
+++ test.support/test/support.py
@@ -1,5 +1,20 @@
 import unittest
 
+
+TESTFN = '@test'
+
 def run_unittest(*classes):
     for c in classes:
         unittest.run_class(c)
+
+def can_symlink():
+    return False
+
+def skip_unless_symlink(test):
+    """Skip decorator for tests that require functional symlink"""
+    ok = can_symlink()
+    msg = "Requires functional symlink implementation"
+    return test if ok else unittest.skip(msg)(test)
+
+def create_empty_file(name):
+    open(name, "w").close()
