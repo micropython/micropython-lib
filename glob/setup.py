d1be6bd2ab52d892fbb0d6c90d2b1ee7d81956e2
Paul Sokolovsky <pfalcon@users.sourceforge.net>
2014-05-20 00:19:19 +0300

    glob: Update metadata.

--- glob/setup.py
+++ glob/setup.py
@@ -6,13 +6,14 @@ from setuptools import setup
 
 
 setup(name='micropython-glob',
-      version='0.0.1',
-      description='Dummy glob module for MicroPython',
-      long_description='This is a dummy implementation of a module for MicroPython standard library.\nIt contains zero or very little functionality, and primarily intended to\navoid import errors (using idea that even if an application imports a\nmodule, it may be not using it onevery code path, so may work at least\npartially). It is expected that more complete implementation of the module\nwill be provided later. Please help with the development if you are\ninterested in this module.',
+      version='0.5',
+      description='CPython glob module ported to MicroPython',
+      long_description='This is a module ported from CPython standard library to be compatible with\nMicroPython interpreter. Usually, this means applying small patches for\nfeatures not supported (yet, or at all) in MicroPython. Sometimes, heavier\nchanges are required. Note that CPython modules are written with availability\nof vast resources in mind, and may not work for MicroPython ports with\nlimited heap. If you are affected by such a case, please help reimplement\nthe module from scratch.',
       url='https://github.com/micropython/micropython/issues/405',
-      author='MicroPython Developers',
-      author_email='micro-python@googlegroups.com',
+      author='CPython Developers',
+      author_email='python-dev@python.org',
       maintainer='MicroPython Developers',
       maintainer_email='micro-python@googlegroups.com',
-      license='MIT',
-      py_modules=['glob'])
+      license='Python',
+      py_modules=['glob'],
+      install_requires=['micropython-os', 'micropython-re-pcre', 'micropython-fnmatch'])
