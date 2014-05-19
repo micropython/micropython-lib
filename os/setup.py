f8252e6e5a02a7b21a1ce3eff5e5e184134fc4c3
Paul Sokolovsky <pfalcon@users.sourceforge.net>
2014-05-19 22:41:54 +0300

    os: Switch to metadata.txt .

--- os/setup.py
+++ os/setup.py
@@ -1,10 +1,18 @@
-from distutils.core import setup
+import sys
+# Remove current dir from sys.path, otherwise setuptools will peek up our
+# module instead of system.
+sys.path.pop(0)
+from setuptools import setup
+
 
 setup(name='micropython-os',
-      version='0.0.6',
+      version='0.0.7',
       description='os module for MicroPython',
+      long_description="This is a module reimplemented specifically for MicroPython standard library,\nwith efficient and lean design in mind. Note that this module is likely work\nin progress and likely supports just a subset of CPython's corresponding\nmodule. Please help with the development if you are interested in this\nmodule.",
       url='https://github.com/micropython/micropython/issues/405',
-      author='MicroPython Developers',
+      author='Paul Sokolovsky',
       author_email='micro-python@googlegroups.com',
+      maintainer='MicroPython Developers',
+      maintainer_email='micro-python@googlegroups.com',
       license='MIT',
       packages=['os'])
