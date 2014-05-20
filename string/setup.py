ba2fb7a880be6b84869612cf3d5e778b06e3b786
Paul Sokolovsky <pfalcon@users.sourceforge.net>
2014-05-20 22:38:52 +0300

    string: Add basic constant definitions.

---
+++ string/setup.py
@@ -0,0 +1,18 @@
+import sys
+# Remove current dir from sys.path, otherwise setuptools will peek up our
+# module instead of system.
+sys.path.pop(0)
+from setuptools import setup
+
+
+setup(name='micropython-string',
+      version='0.0.1',
+      description='string module for MicroPython',
+      long_description="This is a module reimplemented specifically for MicroPython standard library,\nwith efficient and lean design in mind. Note that this module is likely work\nin progress and likely supports just a subset of CPython's corresponding\nmodule. Please help with the development if you are interested in this\nmodule.",
+      url='https://github.com/micropython/micropython/issues/405',
+      author='MicroPython Developers',
+      author_email='micro-python@googlegroups.com',
+      maintainer='MicroPython Developers',
+      maintainer_email='micro-python@googlegroups.com',
+      license='MIT',
+      py_modules=['string'])
