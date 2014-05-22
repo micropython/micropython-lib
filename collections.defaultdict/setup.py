3df5927825c697dd103a2828709ac3d9711d8a9b
Paul Sokolovsky <pfalcon@users.sourceforge.net>
2014-05-23 01:17:53 +0300

    collections.defaultdict: Switch to metadata.txt .

--- collections.defaultdict/setup.py
+++ collections.defaultdict/setup.py
@@ -1,10 +1,18 @@
-from distutils.core import setup
+import sys
+# Remove current dir from sys.path, otherwise setuptools will peek up our
+# module instead of system.
+sys.path.pop(0)
+from setuptools import setup
+
 
 setup(name='micropython-collections.defaultdict',
-      version='0.2',
+      version='0.2.1',
       description='collections.defaultdict module for MicroPython',
+      long_description="This is a module reimplemented specifically for MicroPython standard library,\nwith efficient and lean design in mind. Note that this module is likely work\nin progress and likely supports just a subset of CPython's corresponding\nmodule. Please help with the development if you are interested in this\nmodule.",
       url='https://github.com/micropython/micropython/issues/405',
       author='Paul Sokolovsky',
       author_email='micro-python@googlegroups.com',
+      maintainer='MicroPython Developers',
+      maintainer_email='micro-python@googlegroups.com',
       license='MIT',
       packages=['collections'])
