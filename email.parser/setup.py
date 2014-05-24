105489b7b45265fa5d4a931388ce32c687b70235
Paul Sokolovsky <pfalcon@users.sourceforge.net>
2014-05-24 16:09:11 +0300

    email.parser: Add metadata.

---
+++ email.parser/setup.py
@@ -0,0 +1,19 @@
+import sys
+# Remove current dir from sys.path, otherwise setuptools will peek up our
+# module instead of system.
+sys.path.pop(0)
+from setuptools import setup
+
+
+setup(name='micropython-email.parser',
+      version='0.5',
+      description='CPython email.parser module ported to MicroPython',
+      long_description='This is a module ported from CPython standard library to be compatible with\nMicroPython interpreter. Usually, this means applying small patches for\nfeatures not supported (yet, or at all) in MicroPython. Sometimes, heavier\nchanges are required. Note that CPython modules are written with availability\nof vast resources in mind, and may not work for MicroPython ports with\nlimited heap. If you are affected by such a case, please help reimplement\nthe module from scratch.',
+      url='https://github.com/micropython/micropython/issues/405',
+      author='CPython Developers',
+      author_email='python-dev@python.org',
+      maintainer='MicroPython Developers',
+      maintainer_email='micro-python@googlegroups.com',
+      license='Python',
+      packages=['email'],
+      install_requires=['micropython-warnings', 'micropython-email.feedparser', 'micropython-email.message', 'micropython-email.internal'])
