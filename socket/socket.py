9b3688f98aa34b20f026154177a5506058f6bcd7
Paul Sokolovsky <pfalcon@users.sourceforge.net>
2014-05-25 22:50:58 +0300

    socket: Implement create_connection(), dummy socket.sendall().

--- socket/socket.py
+++ socket/socket.py
@@ -1,1 +1,20 @@
-from microsocket import *
+from microsocket import getaddrinfo
+import microsocket as _socket
+
+def create_connection(addr, timeout=None, source_address=None):
+    s = socket()
+    #print("Address:", addr)
+    ais = getaddrinfo(addr[0], addr[1])
+    #print("Address infos:", ais)
+    for ai in ais:
+        try:
+            s.connect(ai[4])
+            return s
+        except:
+            pass
+
+
+class socket(_socket.socket):
+
+    def sendall(self, *args):
+        return self.send(*args)
