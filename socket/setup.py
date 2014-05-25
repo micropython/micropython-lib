5c1b7e4885280d6f8d168cb2e0abc717dcaf6490
Paul Sokolovsky <pfalcon@users.sourceforge.net>
2014-05-25 22:51:52 +0300

    socket: Switch to metadata.txt.

--- socket/setup.py
+++ socket/setup.py
@@ -1,24 +1,18 @@
 import sys
-# Remove current dir from sys.path, otherwise distutils will peek up our
+# Remove current dir from sys.path, otherwise setuptools will peek up our
 # module instead of system.
 sys.path.pop(0)
 from setuptools import setup
 
 
-def desc_dummy(name):
-    return 'Dummy %s module to MicroPython' % name
-def desc_cpython(name):
-    return 'CPython %s module ported to MicroPython' % name
-def desc_upython(name):
-    return '%s module for MicroPython' % name
-
-NAME = 'socket'
-
-setup(name='micropython-' + NAME,
-      version='0.0.1',
-      description=desc_upython(NAME),
+setup(name='micropython-socket',
+      version='0.0.2',
+      description='socket module for MicroPython',
+      long_description="This is a module reimplemented specifically for MicroPython standard library,\nwith efficient and lean design in mind. Note that this module is likely work\nin progress and likely supports just a subset of CPython's corresponding\nmodule. Please help with the development if you are interested in this\nmodule.",
       url='https://github.com/micropython/micropython/issues/405',
-      author='MicroPython Developers',
+      author='Paul Sokolovsky',
       author_email='micro-python@googlegroups.com',
+      maintainer='MicroPython Developers',
+      maintainer_email='micro-python@googlegroups.com',
       license='MIT',
-      py_modules=[NAME])
+      py_modules=['socket'])
