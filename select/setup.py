286ead8b99281c0f1e8a4a79be2252b63587d353
Paul Sokolovsky <pfalcon@users.sourceforge.net>
2014-05-31 15:29:27 +0300

    select: Convert to metadata.txt.

--- select/setup.py
+++ select/setup.py
@@ -1,15 +1,19 @@
 import sys
-# Remove current dir from sys.path, otherwise distutils will peek up our
-# copy module instead of system.
+# Remove current dir from sys.path, otherwise setuptools will peek up our
+# module instead of system.
 sys.path.pop(0)
 from setuptools import setup
 
+
 setup(name='micropython-select',
-      version='0.0.5',
-      description='select module to MicroPython',
+      version='0.0.6',
+      description='select module for MicroPython',
+      long_description="This is a module reimplemented specifically for MicroPython standard library,\nwith efficient and lean design in mind. Note that this module is likely work\nin progress and likely supports just a subset of CPython's corresponding\nmodule. Please help with the development if you are interested in this\nmodule.",
       url='https://github.com/micropython/micropython/issues/405',
       author='Paul Sokolovsky',
       author_email='micro-python@googlegroups.com',
+      maintainer='MicroPython Developers',
+      maintainer_email='micro-python@googlegroups.com',
       license='MIT',
-      install_requires=['micropython-os'],
-      py_modules=['select'])
+      py_modules=['select'],
+      install_requires=['micropython-os'])
