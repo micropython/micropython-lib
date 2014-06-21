719d2b2081940f51d332e1ca60c16aad592630c3
Paul Sokolovsky <pfalcon@users.sourceforge.net>
2014-06-21 03:03:19 +0300

    sqlite3: Add metadata.

---
+++ sqlite3/setup.py
@@ -0,0 +1,18 @@
+import sys
+# Remove current dir from sys.path, otherwise setuptools will peek up our
+# module instead of system.
+sys.path.pop(0)
+from setuptools import setup
+
+
+setup(name='micropython-sqlite3',
+      version='0.2',
+      description='sqlite3 module for MicroPython',
+      long_description="This is a module reimplemented specifically for MicroPython standard library,\nwith efficient and lean design in mind. Note that this module is likely work\nin progress and likely supports just a subset of CPython's corresponding\nmodule. Please help with the development if you are interested in this\nmodule.",
+      url='https://github.com/micropython/micropython/issues/405',
+      author='Paul Sokolovsky',
+      author_email='micro-python@googlegroups.com',
+      maintainer='MicroPython Developers',
+      maintainer_email='micro-python@googlegroups.com',
+      license='MIT',
+      py_modules=['sqlite3'])
