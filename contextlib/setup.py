92ef77c3d2b4dbd1e4ac257860327ebdaa967c33
nvbn <nvbn.rm@gmail.com>
2014-12-24 21:28:23 +0300

    contextlib: Make compatible with micropython

--- contextlib/setup.py
+++ contextlib/setup.py
@@ -6,13 +6,13 @@ from setuptools import setup
 
 
 setup(name='micropython-contextlib',
-      version='3.4.2-0',
-      description='Port of contextlib for micropython',
-      long_description='Port of contextlib for micropython',
-      author='MicroPython Developers',
-      author_email='micro-python@googlegroups.com',
+      version='3.4.2-1',
+      description='CPython contextlib module ported to MicroPython',
+      long_description='This is a module ported from CPython standard library to be compatible with\nMicroPython interpreter. Usually, this means applying small patches for\nfeatures not supported (yet, or at all) in MicroPython. Sometimes, heavier\nchanges are required. Note that CPython modules are written with availability\nof vast resources in mind, and may not work for MicroPython ports with\nlimited heap. If you are affected by such a case, please help reimplement\nthe module from scratch.',
+      url='https://github.com/micropython/micropython/issues/405',
+      author='CPython Developers',
+      author_email='python-dev@python.org',
       maintainer='MicroPython Developers',
       maintainer_email='micro-python@googlegroups.com',
-      license='MIT',
-      packages=['contextlib'],
-      install_requires=['micropython-unittest'])
+      license='Python',
+      py_modules=['contextlib'])
