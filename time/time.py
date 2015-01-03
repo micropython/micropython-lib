8c7a4194d7f2ffb7e1f55e7bad715a928ef49a6b
Paul Sokolovsky <pfalcon@users.sourceforge.net>
2015-01-04 00:40:33 +0200

    time: Initial cut at implementing strftime() (via ffi).

--- time/time.py
+++ time/time.py
@@ -1,1 +1,26 @@
 from utime import *
+import ffi
+import _libc
+import array
+
+libc = _libc.get()
+
+# struct tm *gmtime(const time_t *timep);
+# struct tm *localtime(const time_t *timep);
+# size_t strftime(char *s, size_t max, const char *format,
+#                       const struct tm *tm);
+gmtime_ = libc.func("P", "gmtime", "P")
+localtime_ = libc.func("P", "localtime", "P")
+strftime_ = libc.func("i", "strftime", "sisP")
+
+
+def strftime(format, t=None):
+    if t is None:
+        t = time()
+
+    t = int(t)
+    a = array.array('i', [t])
+    tm_p = localtime_(a)
+    buf = bytearray(32)
+    l = strftime_(buf, 32, format, tm_p)
+    return str(buf[:l], "utf-8")
