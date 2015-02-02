94b2afdf8b7bb19d5b6c12921b7b39eb2d5b73dd
Paul Sokolovsky <pfalcon@users.sourceforge.net>
2015-02-02 20:20:23 +0200

    utarfile: Metadata and example.

---
+++ utarfile/setup.py
@@ -0,0 +1,18 @@
+import sys
+# Remove current dir from sys.path, otherwise setuptools will peek up our
+# module instead of system.
+sys.path.pop(0)
+from setuptools import setup
+
+
+setup(name='micropython-utarfile',
+      version='0.1',
+      description='utarfile module for MicroPython',
+      long_description='Lightweight tarfile module subset',
+      url='https://github.com/micropython/micropython/issues/405',
+      author='Paul Sokolovsky',
+      author_email='micro-python@googlegroups.com',
+      maintainer='MicroPython Developers',
+      maintainer_email='micro-python@googlegroups.com',
+      license='MIT',
+      py_modules=['utarfile'])
