93cdad6c36fb3f0108f71f083bf3d85d901bc22e
slush0 <slush@satoshilabs.com>
2015-02-08 17:39:24 +0100

    Added test for odd-length string

--- binascii/binascii.py
+++ binascii/binascii.py
@@ -1,6 +1,9 @@
 from ubinascii import hexlify
 
 def unhexlify(data):
+    if len(data) % 2 != 0:
+        raise Exception("Odd-length string")
+
     chunks = [ data[i:i+2] for i in range(0, len(data), 2) ]
     return b''.join(map(lambda x: int(x, 16).to_bytes(1), chunks))
 
