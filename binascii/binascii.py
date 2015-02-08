927bfc3f942ce1952246068fe51fb0b9b3f690d6
slush0 <slush@satoshilabs.com>
2015-02-08 17:16:15 +0100

    Implementation of unhexlify, roughly 60x slower than native algo in python3

--- binascii/binascii.py
+++ binascii/binascii.py
@@ -1,3 +1,12 @@
+from ubinascii import hexlify
+
+def unhexlify(data):
+    chunks = [ data[i:i+2] for i in range(0, len(data), 2) ]
+    return b''.join(map(lambda x: int(x, 16).to_bytes(1), chunks))
+
+b2a_hex = hexlify
+a2b_hex = unhexlify
+
 def b2a_base64():
     raise NotImplementError
 
