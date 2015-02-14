6456fca976c6388a05d77316368f856b63f26a1b
Paul Sokolovsky <pfalcon@users.sourceforge.net>
2015-02-15 00:45:59 +0800

    upip: Add setup.py.

---
+++ upip/setup.py
@@ -0,0 +1,18 @@
+import sys
+# Remove current dir from sys.path, otherwise setuptools will peek up our
+# module instead of system.
+sys.path.pop(0)
+from setuptools import setup
+
+
+setup(name='micropython-upip',
+      version='0.1',
+      description='Simple package manager for MicroPython.',
+      long_description='Simple package manager for MicroPython, targetting to be self-hosted (but not yet there). Compatible only with packages without custom setup.py code.',
+      url='https://github.com/micropython/micropython/issues/405',
+      author='Paul Sokolovsky',
+      author_email='micro-python@googlegroups.com',
+      maintainer='MicroPython Developers',
+      maintainer_email='micro-python@googlegroups.com',
+      license='MIT',
+      py_modules=['upip', 'upip_errno', 'upip_gzip', 'upip_os', 'upip_os_path', 'upip_utarfile'])
