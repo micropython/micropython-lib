ffc30e8c68afb09c5bc8c0901baeeaaf0d3a8f24
Paul Sokolovsky <pfalcon@users.sourceforge.net>
2015-02-22 14:07:40 +0200

    warnings: Add example.

---
+++ warnings/example_warn.py
@@ -0,0 +1,6 @@
+import warnings
+
+warnings.warn('block_size of %d seems too small; using our '
+                               'default of %d.',
+                               RuntimeError, 2)
+#                               RuntimeWarning, 2)
