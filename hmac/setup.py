2f2aeae14ba6af9abc8e666d41cd834f08b149b4
Paul Sokolovsky <pfalcon@users.sourceforge.net>
2015-02-22 13:53:32 +0200

    hmac: Depends on warnings module.

--- hmac/setup.py
+++ hmac/setup.py
@@ -6,7 +6,7 @@ from setuptools import setup
 
 
 setup(name='micropython-hmac',
-      version='0.1',
+      version='3.4.2-1',
       description='CPython hmac module ported to MicroPython',
       long_description='This is a module ported from CPython standard library to be compatible with\nMicroPython interpreter. Usually, this means applying small patches for\nfeatures not supported (yet, or at all) in MicroPython. Sometimes, heavier\nchanges are required. Note that CPython modules are written with availability\nof vast resources in mind, and may not work for MicroPython ports with\nlimited heap. If you are affected by such a case, please help reimplement\nthe module from scratch.',
       url='https://github.com/micropython/micropython/issues/405',
@@ -15,5 +15,5 @@ setup(name='micropython-hmac',
       maintainer='MicroPython Developers',
       maintainer_email='micro-python@googlegroups.com',
       license='Python',
-      install_requires=['micropython-hashlib'],
-      py_modules=['hmac'])
+      py_modules=['hmac'],
+      install_requires=['micropython-warnings', 'micropython-hashlib'])
