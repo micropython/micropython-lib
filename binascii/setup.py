6a7a9d2952acd86b62c27225990ce88f22ebd01b
Paul Sokolovsky <pfalcon@users.sourceforge.net>
2015-02-22 18:14:24 +0200

    binascii: Release 2.4.0-1.

--- binascii/setup.py
+++ binascii/setup.py
@@ -6,12 +6,12 @@ from setuptools import setup
 
 
 setup(name='micropython-binascii',
-      version='0.0.2',
-      description='Dummy binascii module for MicroPython',
-      long_description='This is a dummy implementation of a module for MicroPython standard library.\nIt contains zero or very little functionality, and primarily intended to\navoid import errors (using idea that even if an application imports a\nmodule, it may be not using it onevery code path, so may work at least\npartially). It is expected that more complete implementation of the module\nwill be provided later. Please help with the development if you are\ninterested in this module.',
+      version='2.4.0-1',
+      description='PyPy binascii module ported to MicroPython',
+      long_description='This is a module ported from PyPy standard library to be compatible with\nMicroPython interpreter. Usually, this means applying small patches for\nfeatures not supported (yet, or at all) in MicroPython. Sometimes, heavier\nchanges are required. Note that CPython modules are written with availability\nof vast resources in mind, and may not work for MicroPython ports with\nlimited heap. If you are affected by such a case, please help reimplement\nthe module from scratch.',
       url='https://github.com/micropython/micropython/issues/405',
-      author='MicroPython Developers',
-      author_email='micro-python@googlegroups.com',
+      author='PyPy Developers',
+      author_email='pypy-dev@python.org',
       maintainer='MicroPython Developers',
       maintainer_email='micro-python@googlegroups.com',
       license='MIT',
