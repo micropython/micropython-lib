24d04d6fd7043adcf7363f6d0e1afb3a429ede5d
Paul Sokolovsky <pfalcon@users.sourceforge.net>
2015-02-22 14:07:23 +0200

    warnings: Release 0.1.

--- warnings/setup.py
+++ warnings/setup.py
@@ -1,10 +1,18 @@
-from distutils.core import setup
+import sys
+# Remove current dir from sys.path, otherwise setuptools will peek up our
+# module instead of system.
+sys.path.pop(0)
+from setuptools import setup
+
 
 setup(name='micropython-warnings',
-      version='0.0.1',
-      description='Dummy warnings package for MicroPython',
+      version='0.1',
+      description='warnings module for MicroPython',
+      long_description="This is a module reimplemented specifically for MicroPython standard library,\nwith efficient and lean design in mind. Note that this module is likely work\nin progress and likely supports just a subset of CPython's corresponding\nmodule. Please help with the development if you are interested in this\nmodule.",
       url='https://github.com/micropython/micropython/issues/405',
       author='MicroPython Developers',
       author_email='micro-python@googlegroups.com',
+      maintainer='MicroPython Developers',
+      maintainer_email='micro-python@googlegroups.com',
       license='MIT',
       py_modules=['warnings'])
