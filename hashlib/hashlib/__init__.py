654c7d288603f7dae09eb09b57fb67b38c7ac6c3
Paul Sokolovsky <pfalcon@users.sourceforge.net>
2015-03-01 07:35:24 +0200

    hashlib: Selectively import individual classes to top-level package.

--- hashlib/hashlib/__init__.py
+++ hashlib/hashlib/__init__.py
@@ -1,2 +1,2 @@
-from .sha256 import *
-from .sha512 import *
+from .sha256 import sha224, sha256
+from .sha512 import sha384, sha512
--- hashlib/setup.py
+++ hashlib/setup.py
@@ -6,7 +6,7 @@ from setuptools import setup
 
 
 setup(name='micropython-hashlib',
-      version='2.4.0-1',
+      version='2.4.0-2',
       description='PyPy hashlib module ported to MicroPython',
       long_description='This is a module ported from PyPy standard library to be compatible with\nMicroPython interpreter. Usually, this means applying small patches for\nfeatures not supported (yet, or at all) in MicroPython. Sometimes, heavier\nchanges are required. Note that CPython modules are written with availability\nof vast resources in mind, and may not work for MicroPython ports with\nlimited heap. If you are affected by such a case, please help reimplement\nthe module from scratch.',
       url='https://github.com/micropython/micropython/issues/405',
