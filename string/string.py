6e64994ec27293bce2da7a1e2da88993c08dc2e8
Paul Sokolovsky <pfalcon@users.sourceforge.net>
2015-03-28 19:48:36 +0200

    string: Add implementation of translate() method.
    As MicroPython doesn't have str.translate() method, here it's implemented
    as string module function.

--- string/string.py
+++ string/string.py
@@ -8,3 +8,19 @@ hexdigits = digits + 'abcdef' + 'ABCDEF'
 octdigits = '01234567'
 punctuation = """!"#$%&'()*+,-./:;<=>?@[\]^_`{|}~"""
 printable = digits + ascii_letters + punctuation + whitespace
+
+
+def translate(s, map):
+    import io
+    sb = io.StringIO()
+    for c in s:
+        v = ord(c)
+        if v in map:
+            v = map[v]
+            if isinstance(v, int):
+                sb.write(chr(v))
+            elif v is not None:
+                sb.write(v)
+        else:
+            sb.write(c)
+    return sb.getvalue()
