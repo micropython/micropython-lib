d86720fe9fb02c9681cd2d7398122f37144fe6d4
Paul Sokolovsky <pfalcon@users.sourceforge.net>
2015-05-08 01:06:14 +0300

    upip: bootstrap_upip.sh: Perform processing in /tmp.

--- upip/bootstrap_upip.sh
+++ upip/bootstrap_upip.sh
@@ -1,6 +1,12 @@
 # This script performs bootstrap installation of upip package manager from PyPI
 # All the other packages can be installed using it.
 
+if [ -z "$TMPDIR" ]; then
+    cd /tmp
+else
+    cd $TMPDIR
+fi
+
 # Remove any stale old version
 rm -rf micropython-upip-*
 wget -nd -r -l1 https://pypi.python.org/pypi/micropython-upip/ --accept-regex ".*pypi.python.org/packages/source/.*.gz" --reject=html
