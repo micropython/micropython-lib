ae1a4570a69e3e3f1ddf17e886e3336477dd0cfd
Paul Sokolovsky <pfalcon@users.sourceforge.net>
2015-06-20 19:28:21 +0300

    multiprocessing: Add metadata.txt .

--- multiprocessing/setup.py
+++ multiprocessing/setup.py
@@ -1,15 +1,19 @@
 import sys
-# Remove current dir from sys.path, otherwise distutils will peek up our
+# Remove current dir from sys.path, otherwise setuptools will peek up our
 # module instead of system.
 sys.path.pop(0)
 from setuptools import setup
 
+
 setup(name='micropython-multiprocessing',
-      version='0.0.2',
-      description='multiprocessing module to MicroPython',
+      version='0.0.3',
+      description='multiprocessing module for MicroPython',
+      long_description="This is a module reimplemented specifically for MicroPython standard library,\nwith efficient and lean design in mind. Note that this module is likely work\nin progress and likely supports just a subset of CPython's corresponding\nmodule. Please help with the development if you are interested in this\nmodule.",
       url='https://github.com/micropython/micropython/issues/405',
       author='Paul Sokolovsky',
       author_email='micro-python@googlegroups.com',
+      maintainer='MicroPython Developers',
+      maintainer_email='micro-python@googlegroups.com',
       license='MIT',
-      install_requires=['micropython-os', 'micropython-select', 'micropython-pickle'],
-      py_modules=['multiprocessing'])
+      py_modules=['multiprocessing'],
+      install_requires=['micropython-os', 'micropython-select', 'micropython-pickle'])
