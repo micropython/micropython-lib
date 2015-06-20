7fe611172f2f38bbdc81ad031e8c67b7b85a88f3
Paul Sokolovsky <pfalcon@users.sourceforge.net>
2015-06-21 02:02:54 +0300

    uasyncio/benchmark: Default is "medium" test, add CPython support.

--- uasyncio/benchmark/test-ab-light.sh
+++ uasyncio/benchmark/test-ab-light.sh
@@ -4,7 +4,12 @@
 # Apache Bench (ab).
 #
 
-micropython -O test_http_server_light.py &
+#python3.4.2 test_http_server_light.py &
+#micropython -O test_http_server_light.py &
+
+#python3.4.2 test_http_server_medium.py &
+micropython -O -X heapsize=200wK test_http_server_medium.py &
+
 sleep 1
 
 ab -n10000 -c100 http://localhost:8081/
