21f8e09be8e5089bc0189d5609ad6363ac5e7420
Paul Sokolovsky <pfalcon@users.sourceforge.net>
2015-07-05 22:24:35 +0300

    test.support: Implement helpers required to run test_pep380.py.

--- test.support/test/support.py
+++ test.support/test/support.py
@@ -1,4 +1,8 @@
+import sys
+import io
 import unittest
+import gc
+import contextlib
 
 
 TESTFN = '@test'
@@ -25,3 +29,31 @@ def skip_unless_symlink(test):
 
 def create_empty_file(name):
     open(name, "w").close()
+
+@contextlib.contextmanager
+def disable_gc():
+    have_gc = gc.isenabled()
+    gc.disable()
+    try:
+        yield
+    finally:
+        if have_gc:
+            gc.enable()
+
+def gc_collect():
+    gc.collect()
+    gc.collect()
+    gc.collect()
+
+@contextlib.contextmanager
+def captured_output(stream_name):
+    org = getattr(sys, stream_name)
+    buf = io.StringIO()
+    setattr(sys, stream_name, buf)
+    try:
+        yield buf
+    finally:
+        setattr(sys, stream_name, org)
+
+def captured_stderr():
+    return captured_output("stderr")
