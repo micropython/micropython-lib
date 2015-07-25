29a9f563728dacc18e27d7fb9d629efdba345868
Tom Soulanille <soul@prama.com>
2015-07-24 21:13:26 -0700

    unittest: fill out asserts and test them

--- unittest/test_unittest.py
+++ unittest/test_unittest.py
@@ -1,20 +1,64 @@
 import unittest
 
-class TestStringMethods(unittest.TestCase):
+class TestUnittestAssertions(unittest.TestCase):
 
-  def test_upper(self):
-      self.assertEqual('foo'.upper(), 'FOO')
+    def testFail(self):
+        with self.assertRaises(AssertionError):
+            self.fail('failure')
 
-  def test_isupper(self):
-      self.assertTrue('FOO'.isupper())
-      self.assertFalse('Foo'.isupper())
+    def testEqual(self):
+        self.assertEqual(0,0)
+        self.assertEqual([0,1,2], [0,1,2])
+        with self.assertRaises(AssertionError):
+            self.assertEqual(0,None)
+        with self.assertRaises(AssertionError):
+            self.assertEqual([0,1,2], [1,2,3])
 
-  def test_split(self):
-      s = 'hello world'
-      self.assertEqual(s.split(), ['hello', 'world'])
-      # check that s.split fails when the separator is not a string
-      with self.assertRaises(TypeError):
-          s.split(2)
+    def testNotEqual(self):
+        self.assertNotEqual([0,1,2], [0,2,1])
+        with self.assertRaises(AssertionError):
+            self.assertNotEqual(0,0)
+        with self.assertRaises(AssertionError):
+            self.assertNotEqual([0,1,2], [0,1,2])
+
+    def testIs(self):
+        self.assertIs(None, None)
+        with self.assertRaises(AssertionError):
+            self.assertIs([1,2,3], [1,2,3])
+
+    def testIsNot(self):
+        self.assertIsNot([1,2,3], [1,2,3])
+        with self.assertRaises(AssertionError):
+            self.assertIsNot(None, None)
+
+    def testTrue(self):
+        self.assertTrue(True)
+        with self.assertRaises(AssertionError):
+            self.assertTrue(False)
+
+    def testFalse(self):
+        self.assertFalse(False)
+        with self.assertRaises(AssertionError):
+            self.assertFalse(True)
+
+    def testIn(self):
+        self.assertIn('t', 'cat')
+        with self.assertRaises(AssertionError):
+            self.assertIn('x', 'cat')
+
+    def testIsInstance(self):
+        self.assertIsInstance('cat', str)
+        with self.assertRaises(AssertionError):
+            self.assertIsInstance(7, str)
+
+    def testRaises(self):
+        with self.assertRaises(ZeroDivisionError):
+            1/0
+            pass
+
+    @unittest.skip('test of skipping')
+    def testSkip(self):
+        self.assertFail('this should be skipped')
 
 if __name__ == '__main__':
     unittest.main()
