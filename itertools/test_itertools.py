9a81d0ec25bcdebeecf3e4040741290e97fba731
Paul Sokolovsky <pfalcon@users.sourceforge.net>
2015-08-25 21:01:43 +0300

    itertools: Turn test_itertools.py into real test.

--- itertools/test_itertools.py
+++ itertools/test_itertools.py
@@ -1,5 +1,5 @@
 import itertools
 
-print(list(itertools.islice(list(range(10)), 4)))
-print(list(itertools.islice(list(range(10)), 2, 6)))
-print(list(itertools.islice(list(range(10)), 2, 6, 2)))
+assert list(itertools.islice(list(range(10)), 4)) == [0, 1, 2, 3]
+assert list(itertools.islice(list(range(10)), 2, 6)) == [2, 3, 4, 5]
+assert list(itertools.islice(list(range(10)), 2, 6, 2)) == [2, 4]
