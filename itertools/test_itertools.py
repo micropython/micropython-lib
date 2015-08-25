47c4ef54bcd2f51dc2bf0bc345f111889d4418e1
Paul Sokolovsky <pfalcon@users.sourceforge.net>
2015-08-25 21:18:25 +0300

    itertools: test_itertools.py: Test for islice() on generator.

--- itertools/test_itertools.py
+++ itertools/test_itertools.py
@@ -3,3 +3,9 @@ import itertools
 assert list(itertools.islice(list(range(10)), 4)) == [0, 1, 2, 3]
 assert list(itertools.islice(list(range(10)), 2, 6)) == [2, 3, 4, 5]
 assert list(itertools.islice(list(range(10)), 2, 6, 2)) == [2, 4]
+
+def g():
+    while True:
+        yield 123
+
+assert list(itertools.islice(g(), 5)) == [123, 123, 123, 123, 123]
