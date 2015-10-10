db6c6e1a55418faec3ef5c840153ed5c1a9faddd
Paul Sokolovsky <pfalcon@users.sourceforge.net>
2015-10-10 13:10:11 +0300

    xmltok: Add test.

---
+++ xmltok/test_xmltok.py
@@ -0,0 +1,21 @@
+import xmltok
+
+expected = [
+('PI', 'xml'),
+('ATTR', ('', 'version'), '1.0'),
+('START_TAG', ('s', 'Envelope')),
+('ATTR', ('xmlns', 's'), 'http://schemas.xmlsoap.org/soap/envelope/'),
+('ATTR', ('s', 'encodingStyle'), 'http://schemas.xmlsoap.org/soap/encoding/'),
+('START_TAG', ('s', 'Body')),
+('START_TAG', ('u', 'GetConnectionTypeInfo')),
+('ATTR', ('xmlns', 'u'), 'urn:schemas-upnp-org:service:WANIPConnection:1'),
+('TEXT', 'foo bar\n  baz\n  \n'),
+('END_TAG', ('u', 'GetConnectionTypeInfo')),
+('END_TAG', ('s', 'Body')),
+('END_TAG', ('s', 'Envelope')),
+]
+
+ex = iter(expected)
+for i in xmltok.tokenize(open("test.xml")):
+    #print(i)
+    assert i == next(ex)
