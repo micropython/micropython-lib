773f88f25392b429064dad72c3131f4017dbcf02
Nico <nicolas.jouanin@gmail.com>
2015-11-15 15:25:11 +0100

    urllib.parse : parse.py requires collections.defaultdict class. This class is available in the collections.defaultdict module, which was not listed in the dependencies.

--- urllib.parse/setup.py
+++ urllib.parse/setup.py
@@ -6,7 +6,7 @@ from setuptools import setup
 
 
 setup(name='micropython-urllib.parse',
-      version='0.5',
+      version='0.5.1',
       description='CPython urllib.parse module ported to MicroPython',
       long_description='This is a module ported from CPython standard library to be compatible with\nMicroPython interpreter. Usually, this means applying small patches for\nfeatures not supported (yet, or at all) in MicroPython. Sometimes, heavier\nchanges are required. Note that CPython modules are written with availability\nof vast resources in mind, and may not work for MicroPython ports with\nlimited heap. If you are affected by such a case, please help reimplement\nthe module from scratch.',
       url='https://github.com/micropython/micropython/issues/405',
@@ -16,4 +16,4 @@ setup(name='micropython-urllib.parse',
       maintainer_email='micro-python@googlegroups.com',
       license='Python',
       packages=['urllib'],
-      install_requires=['micropython-re-pcre', 'micropython-collections'])
+      install_requires=['micropython-re-pcre', 'micropython-collections', 'micropython-collections.defaultdict'])
