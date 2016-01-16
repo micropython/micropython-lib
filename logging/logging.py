c607efe88a053d1d72a3b5ae176c417af08e1c51
Paul Sokolovsky <pfalcon@users.sourceforge.net>
2016-01-17 00:37:50 +0200

    logging: Remove outdated comment.

--- logging/logging.py
+++ logging/logging.py
@@ -1,9 +1,3 @@
-# Dummy file to preclude import errors
-# Should be reimplemented for MicroPython
-# Reason:
-# Basic, useful module, by CPython impl depends on module "string" which
-# uses metaclasses.
-
 import sys
 
 CRITICAL = 50
