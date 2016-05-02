901456fce4008bc405e22cd2d1dcede012863232
Paul Sokolovsky <pfalcon@users.sourceforge.net>
2016-05-02 14:08:15 +0300

    collections: Release 0.1.1.

--- collections/setup.py
+++ collections/setup.py
@@ -6,7 +6,7 @@ from setuptools import setup
 
 
 setup(name='micropython-collections',
-      version='0.1',
+      version='0.1.1',
       description='collections module for MicroPython',
       long_description="This is a module reimplemented specifically for MicroPython standard library,\nwith efficient and lean design in mind. Note that this module is likely work\nin progress and likely supports just a subset of CPython's corresponding\nmodule. Please help with the development if you are interested in this\nmodule.",
       url='https://github.com/micropython/micropython/issues/405',
