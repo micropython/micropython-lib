5ce27099a93aa7f795f57b2f50bfa983a512fbb5
Paul Sokolovsky <pfalcon@users.sourceforge.net>
2016-06-10 23:09:38 +0300

    traceback: Add print_exception(), print_exc().

--- traceback/traceback.py
+++ traceback/traceback.py
@@ -1,5 +1,15 @@
+import sys
+
 def format_tb(tb, limit):
     return ["traceback.format_tb() not implemented\n"]
 
 def format_exception_only(type, value):
     return [repr(value) + "\n"]
+
+def print_exception(t, e, tb, limit=None, file=None, chain=True):
+    if file is None:
+        file = sys.stdout
+    sys.print_exception(e, file)
+
+def print_exc(limit=None, file=None, chain=True):
+    print_exception(*sys.exc_info(), limit=limit, file=file, chain=chain)
