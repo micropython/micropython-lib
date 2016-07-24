2fecbd03121f117f21e7c7cbc6548a5fca9521d6
Paul Sokolovsky <pfalcon@users.sourceforge.net>
2016-07-24 03:26:52 +0300

    umqtt.robust: Initial version of client with auto-reconnect on errors.

---
+++ umqtt.robust/umqtt/robust.py
@@ -0,0 +1,43 @@
+import utime
+from . import simple
+
+class MQTTClient(simple.MQTTClient):
+
+    DELAY = 2
+    DEBUG = False
+
+    def delay(self, i):
+        utime.sleep(self.DELAY)
+
+    def log(self, in_reconnect, exc):
+        if self.DEBUG:
+            if in_reconnect:
+                print("mqtt reconnect: %r" % e)
+            else:
+                print("mqtt: %r" % e)
+
+    def reconnect(self, i):
+        i = 0
+        while 1:
+            try:
+                return super().connect(False)
+            except OSError as e:
+                self.log(True, e)
+                i += 1
+                self.delay(i)
+
+    def publish(self, topic, msg, qos=0, retain=False):
+        while 1:
+            try:
+                return super().publish(topic, msg, qos, retain)
+            except OSError as e:
+                self.log(False, e)
+            self.reconnect()
+
+    def wait_msg(self):
+        while 1:
+            try:
+                return super().wait_msg()
+            except OSError as e:
+                self.log(False, e)
+            self.reconnect()
