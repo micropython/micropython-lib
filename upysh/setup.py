1e83b27b9d2d4262cc27153aee9585f6210aea0c
Paul Sokolovsky <pfalcon@users.sourceforge.net>
2016-08-06 15:11:28 +0300

    upysh: Release 0.6.

--- upysh/setup.py
+++ upysh/setup.py
@@ -6,11 +6,11 @@ from setuptools import setup
 
 
 setup(name='micropython-upysh',
-      version='0.5',
+      version='0.6',
       description='Minimalistic file shell using native Python syntax.',
       long_description='Minimalistic file shell using native Python syntax.',
       url='https://github.com/micropython/micropython/issues/405',
-      author='Paul Sokolovsky',
+      author='MicroPython Developers',
       author_email='micro-python@googlegroups.com',
       maintainer='MicroPython Developers',
       maintainer_email='micro-python@googlegroups.com',
