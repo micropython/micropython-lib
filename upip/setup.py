7bc041e620541f3ba72336552ecebb9679f199de
Paul Sokolovsky <pfalcon@users.sourceforge.net>
2016-10-05 00:32:33 +0300

    upip: Release 1.0.

--- upip/setup.py
+++ upip/setup.py
@@ -6,13 +6,13 @@ from setuptools import setup
 
 
 setup(name='micropython-upip',
-      version='0.8',
+      version='1.0',
       description='Simple package manager for MicroPython.',
-      long_description='Simple package manager for MicroPython, targetting to be self-hosted (but not yet there). Compatible only with packages without custom setup.py code.',
+      long_description='Simple self-hosted package manager for MicroPython (requires usocket, ussl, uzlib, uctypes builtin modules). Compatible only with packages without custom setup.py code.',
       url='https://github.com/micropython/micropython/issues/405',
       author='Paul Sokolovsky',
       author_email='micro-python@googlegroups.com',
       maintainer='MicroPython Developers',
       maintainer_email='micro-python@googlegroups.com',
       license='MIT',
-      py_modules=['upip', 'upip_errno', 'upip_gzip', 'upip_utarfile'])
+      py_modules=['upip', 'upip_utarfile'])
