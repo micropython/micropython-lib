94be568a034643c41f15627f6f191bc5c13265ee
Tobias Ammann <tag@adnm.ch>
2016-11-15 18:37:27 +0100

    functools: Make partial wrapper pass on return value.

--- functools/setup.py
+++ functools/setup.py
@@ -7,7 +7,7 @@ sys.path.append("..")
 import optimize_upip
 
 setup(name='micropython-functools',
-      version='0.0.5',
+      version='0.0.6',
       description='functools module for MicroPython',
       long_description="This is a module reimplemented specifically for MicroPython standard library,\nwith efficient and lean design in mind. Note that this module is likely work\nin progress and likely supports just a subset of CPython's corresponding\nmodule. Please help with the development if you are interested in this\nmodule.",
       url='https://github.com/micropython/micropython-lib',
