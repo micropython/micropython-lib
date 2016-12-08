1abe4fc8ed8c42fdafd5d0ef5831a8a33242aa10
Paul Sokolovsky <pfalcon@users.sourceforge.net>
2016-12-08 14:36:35 +0300

    re-pcre: Release 0.2.3.

--- re-pcre/setup.py
+++ re-pcre/setup.py
@@ -7,7 +7,7 @@ sys.path.append("..")
 import optimize_upip
 
 setup(name='micropython-re-pcre',
-      version='0.2.2',
+      version='0.2.3',
       description='re-pcre module for MicroPython',
       long_description="This is a module reimplemented specifically for MicroPython standard library,\nwith efficient and lean design in mind. Note that this module is likely work\nin progress and likely supports just a subset of CPython's corresponding\nmodule. Please help with the development if you are interested in this\nmodule.",
       url='https://github.com/micropython/micropython-lib',
