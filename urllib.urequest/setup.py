abddcd7893891a1a64bfb4dfa4d0d2b3330d9e13
Paul Sokolovsky <pfalcon@users.sourceforge.net>
2016-12-08 14:31:59 +0300

    urllib.urequest: Release 0.4.3.

--- urllib.urequest/setup.py
+++ urllib.urequest/setup.py
@@ -7,7 +7,7 @@ sys.path.append("..")
 import optimize_upip
 
 setup(name='micropython-urllib.urequest',
-      version='0.4.2',
+      version='0.4.3',
       description='urllib.urequest module for MicroPython',
       long_description="This is a module reimplemented specifically for MicroPython standard library,\nwith efficient and lean design in mind. Note that this module is likely work\nin progress and likely supports just a subset of CPython's corresponding\nmodule. Please help with the development if you are interested in this\nmodule.",
       url='https://github.com/micropython/micropython-lib',
