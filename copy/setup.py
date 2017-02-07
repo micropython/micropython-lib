12da3a81a6661d89f041efc57c525684ee5bf584
Paul Sokolovsky <pfalcon@users.sourceforge.net>
2017-02-08 01:23:00 +0300

    copy: Use standard metadata, release is 3.3.3-2.

--- copy/setup.py
+++ copy/setup.py
@@ -1,16 +1,20 @@
 import sys
-# Remove current dir from sys.path, otherwise distutils will peek up our
-# copy module instead of system.
+# Remove current dir from sys.path, otherwise setuptools will peek up our
+# module instead of system's.
 sys.path.pop(0)
 from setuptools import setup
+sys.path.append("..")
+import optimize_upip
 
 setup(name='micropython-copy',
-      version='0.0.2',
+      version='3.3.3-2',
       description='CPython copy module ported to MicroPython',
-      url='https://github.com/micropython/micropython/issues/405',
+      long_description='This is a module ported from CPython standard library to be compatible with\nMicroPython interpreter. Usually, this means applying small patches for\nfeatures not supported (yet, or at all) in MicroPython. Sometimes, heavier\nchanges are required. Note that CPython modules are written with availability\nof vast resources in mind, and may not work for MicroPython ports with\nlimited heap. If you are affected by such a case, please help reimplement\nthe module from scratch.',
+      url='https://github.com/micropython/micropython-lib',
       author='CPython Developers',
+      author_email='python-dev@python.org',
       maintainer='MicroPython Developers',
       maintainer_email='micro-python@googlegroups.com',
       license='Python',
-      install_requires=['micropython-types'],
+      cmdclass={'optimize_upip': optimize_upip.OptimizeUpip},
       py_modules=['copy'])
