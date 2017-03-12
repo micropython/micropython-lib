a355801fb12e3acd0dd251482fad85dd97c68f36
Paul Sokolovsky <pfalcon@users.sourceforge.net>
2017-03-13 00:44:58 +0300

    uasyncio: wait: Remove no longer needed "if", artifact of epoll impl.

--- uasyncio/uasyncio/__init__.py
+++ uasyncio/uasyncio/__init__.py
@@ -55,10 +55,7 @@ class PollEventLoop(EventLoop):
         if DEBUG and __debug__:
             log.debug("poll.wait(%d)", delay)
         # We need one-shot behavior (second arg of 1 to .poll())
-        if delay == -1:
-            res = self.poller.poll(-1, 1)
-        else:
-            res = self.poller.poll(delay, 1)
+        res = self.poller.poll(delay, 1)
         #log.debug("poll result: %s", res)
         # Remove "if res" workaround after
         # https://github.com/micropython/micropython/issues/2716 fixed.
