f30c784dcf83e208bfb425243f756e2197ff1e3c
Paul Sokolovsky <pfalcon@users.sourceforge.net>
2017-05-11 21:21:23 +0300

    pprint: Release 0.0.4.

--- pprint/setup.py
+++ pprint/setup.py
@@ -7,7 +7,7 @@ sys.path.append("..")
 import optimize_upip
 
 setup(name='micropython-pprint',
-      version='0.0.3',
+      version='0.0.4',
       description='Dummy pprint module for MicroPython',
       long_description='This is a dummy implementation of a module for MicroPython standard library.\nIt contains zero or very little functionality, and primarily intended to\navoid import errors (using idea that even if an application imports a\nmodule, it may be not using it onevery code path, so may work at least\npartially). It is expected that more complete implementation of the module\nwill be provided later. Please help with the development if you are\ninterested in this module.',
       url='https://github.com/micropython/micropython-lib',
