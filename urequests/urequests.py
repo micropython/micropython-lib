78ce2a8957326b455abfbf133297a35f29791a9a
Paul Sokolovsky <pfalcon@users.sourceforge.net>
2017-06-14 19:37:08 +0300

    urequests: Add server_hostname param to ussl.wrap_socket() call.

--- urequests/urequests.py
+++ urequests/urequests.py
@@ -53,7 +53,7 @@ def request(method, url, data=None, json=None, headers={}, stream=None):
     s = usocket.socket()
     s.connect(addr)
     if proto == "https:":
-        s = ussl.wrap_socket(s)
+        s = ussl.wrap_socket(s, server_hostname=host)
     s.write(b"%s /%s HTTP/1.0\r\n" % (method, path))
     if not "Host" in headers:
         s.write(b"Host: %s\r\n" % host)
