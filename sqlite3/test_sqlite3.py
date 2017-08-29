5cb3fe10f0a534f1506bde6a4b757e3d2cb0c048
Paul Sokolovsky <pfalcon@users.sourceforge.net>
2017-08-30 00:05:07 +0300

    sqlite3: test_sqlite3: Turn into a real test (with asserts).

--- sqlite3/test_sqlite3.py
+++ sqlite3/test_sqlite3.py
@@ -4,9 +4,16 @@ import sqlite3
 conn = sqlite3.connect(":memory:")
 
 cur = conn.cursor()
-cur.execute("SELECT 1, 'foo', 3.14159 UNION SELECT 3, 3, 3")
+cur.execute("SELECT 1, 'foo', 3.0 UNION SELECT 3, 3, 3")
+
+expected = [(1, 'foo', 3.0), (3, 3, 3)]
+
 while True:
     row = cur.fetchone()
     if row is None:
         break
     print(row)
+    e = expected.pop(0)
+    assert row == e
+
+assert expected == []
