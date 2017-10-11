98bf9edccc61b01d8c11ebb82b893d97eba59a4a
Paul Sokolovsky <pfalcon@users.sourceforge.net>
2017-10-11 20:12:21 +0300

    fnmatch: test_fnmatch: Disable tests for bytes arguments.
    re-pcre doesn't work properly with bytes patterns so far, disable the test
    until later.

--- fnmatch/test_fnmatch.py
+++ fnmatch/test_fnmatch.py
@@ -55,6 +55,7 @@ class FnmatchTestCase(unittest.TestCase):
         check('AbC', 'abc', 0, fnmatchcase)
         check('abc', 'AbC', 0, fnmatchcase)
 
+    @unittest.skip("unsupported on MicroPython")
     def test_bytes(self):
         self.check_match(b'test', b'te*')
         self.check_match(b'test\xff', b'te*\xff')
