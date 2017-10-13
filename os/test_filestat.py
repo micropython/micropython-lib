e9b54606e98078c9c1703a7a243837038a7888ab
Paul Sokolovsky <pfalcon@users.sourceforge.net>
2017-10-13 20:24:35 +0300

    os: test_filestat.py: Use paths relative to module dir.
    Allows to run via test aggregators.

--- os/test_filestat.py
+++ os/test_filestat.py
@@ -1,5 +1,9 @@
 import os
 
 
-assert os.access("test_filestat.py", os.F_OK) == True
-assert os.access("test_filestat.py-not", os.F_OK) == False
+dir = "."
+if "/" in __file__:
+    dir = __file__.rsplit("/", 1)[0]
+
+assert os.access(dir + "/test_filestat.py", os.F_OK) == True
+assert os.access(dir + "/test_filestat.py-not", os.F_OK) == False
