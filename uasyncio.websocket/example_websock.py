754d30019e1416ed2122a19298faa465e3a7d11a
Paul Sokolovsky <pfalcon@users.sourceforge.net>
2017-11-03 21:24:43 +0200

    WIP: uasyncio.websocket: Add example using StreamReader/Writer wrapping.

---
+++ uasyncio.websocket/example_websock.py
@@ -0,0 +1,28 @@
+import uasyncio
+from uasyncio_.websocket import WSReader, WSWriter
+
+
+def echo(reader, writer):
+    # Consume GET line
+    yield from reader.readline()
+
+    reader = yield from WSReader(reader, writer)
+    writer = WSWriter(writer)
+
+    while 1:
+        l = yield from reader.read(256)
+        print(l)
+        if l == b"\r":
+            await writer.awrite(b"\r\n")
+        else:
+            await writer.awrite(l)
+
+
+import logging
+#logging.basicConfig(level=logging.INFO)
+logging.basicConfig(level=logging.DEBUG)
+loop = uasyncio.get_event_loop()
+#loop.create_task(asyncio.start_server(serve, "127.0.0.1", 8081))
+loop.create_task(uasyncio.start_server(echo, "127.0.0.1", 8081))
+loop.run_forever()
+loop.close()
