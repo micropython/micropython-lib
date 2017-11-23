cd829cd1384b9edb2d70705e568831c8d1e88be2
Himura Kazuto <glagol15@gmail.com>
2017-11-23 20:26:43 +0300

    supported connection timeout

--- umqtt.simple/umqtt/simple.py
+++ umqtt.simple/umqtt/simple.py
@@ -52,8 +52,9 @@ class MQTTClient:
         self.lw_qos = qos
         self.lw_retain = retain
 
-    def connect(self, clean_session=True):
+    def connect(self, clean_session=True, timeout=None):
         self.sock = socket.socket()
+        self.sock.settimeout(timeout)
         addr = socket.getaddrinfo(self.server, self.port)[0][-1]
         self.sock.connect(addr)
         if self.ssl:
