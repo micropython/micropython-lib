6c141990ac7a817ed6ca29ffb6e308ff664dbf0c
Manos Tsagkias <tsagias@gmail.com>
2017-12-01 00:00:47 +0100

    urequests: Set Content-Type to application/json when json param is used.
    Otherwise, some servers don't recognize the payload as JSON.

--- urequests/urequests.py
+++ urequests/urequests.py
@@ -71,6 +71,7 @@ def request(method, url, data=None, json=None, headers={}, stream=None):
             assert data is None
             import ujson
             data = ujson.dumps(json)
+            s.write(b"Content-Type: application/json\r\n")
         if data:
             s.write(b"Content-Length: %d\r\n" % len(data))
         s.write(b"\r\n")
