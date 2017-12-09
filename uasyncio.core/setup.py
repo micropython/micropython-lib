a7f8eaa6ed8875c4d278beb3981604b032d515d3
Paul Sokolovsky <pfalcon@users.sourceforge.net>
2017-12-09 18:05:47 +0200

    uasyncio.core: Release 1.6.

--- uasyncio.core/setup.py
+++ uasyncio.core/setup.py
@@ -7,7 +7,7 @@ sys.path.append("..")
 import optimize_upip
 
 setup(name='micropython-uasyncio.core',
-      version='1.5.1',
+      version='1.6',
       description='Lightweight asyncio-like library for MicroPython, built around native Python coroutines. (Core event loop).',
       long_description='Lightweight asyncio-like library for MicroPython, built around native Python coroutines. (Core event loop).',
       url='https://github.com/micropython/micropython-lib',
