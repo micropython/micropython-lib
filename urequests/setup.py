cdea2d94cc761c8819b7e94ddae9fa0f8e91f8a0
Paul Sokolovsky <pfalcon@users.sourceforge.net>
2018-01-14 11:34:44 +0200

    urequests: Release 0.5.2.

--- urequests/setup.py
+++ urequests/setup.py
@@ -7,13 +7,13 @@ sys.path.append("..")
 import optimize_upip
 
 setup(name='micropython-urequests',
-      version='0.5.1',
+      version='0.5.2',
       description='urequests module for MicroPython',
       long_description="This is a module reimplemented specifically for MicroPython standard library,\nwith efficient and lean design in mind. Note that this module is likely work\nin progress and likely supports just a subset of CPython's corresponding\nmodule. Please help with the development if you are interested in this\nmodule.",
       url='https://github.com/micropython/micropython-lib',
       author='Paul Sokolovsky',
       author_email='micro-python@googlegroups.com',
-      maintainer='MicroPython Developers',
+      maintainer='micropython-lib Developers',
       maintainer_email='micro-python@googlegroups.com',
       license='MIT',
       cmdclass={'optimize_upip': optimize_upip.OptimizeUpip},
