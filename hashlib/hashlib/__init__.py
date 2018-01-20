175634b3cdf3a9269f5f6f9f8e76dbd524e24c44
Paul Sokolovsky <pfalcon@users.sourceforge.net>
2018-01-20 11:38:33 +0200

    hashlib: Reuse classes available in ushashlib, extend tests.

--- hashlib/hashlib/__init__.py
+++ hashlib/hashlib/__init__.py
@@ -1,2 +1,22 @@
-from ._sha256 import sha224, sha256
-from ._sha512 import sha384, sha512
+try:
+    import uhashlib
+except ImportError:
+    uhashlib = None
+
+def init():
+    for i in ("sha1", "sha224", "sha256", "sha384", "sha512"):
+        c = getattr(uhashlib, i, None)
+        if not c:
+            c = __import__("_" + i, None, None, (), 1)
+            c = getattr(c, i)
+        globals()[i] = c
+
+init()
+
+
+def new(algo, data=b""):
+    try:
+        c = globals()[algo]
+        return c(data)
+    except KeyError:
+        raise ValueError(algo)
