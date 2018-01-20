73bd87126846dc5ac180b2a376eb4b0ab3d64ad8
Paul Sokolovsky <pfalcon@users.sourceforge.net>
2018-01-20 11:49:53 +0200

    hashlib: Release 2.4.0-4.

--- hashlib/setup.py
+++ hashlib/setup.py
@@ -7,13 +7,13 @@ sys.path.append("..")
 import optimize_upip
 
 setup(name='micropython-hashlib',
-      version='2.4.0-3',
+      version='2.4.0-4',
       description='PyPy hashlib module ported to MicroPython',
       long_description='This is a module ported from PyPy standard library to be compatible with\nMicroPython interpreter. Usually, this means applying small patches for\nfeatures not supported (yet, or at all) in MicroPython. Sometimes, heavier\nchanges are required. Note that CPython modules are written with availability\nof vast resources in mind, and may not work for MicroPython ports with\nlimited heap. If you are affected by such a case, please help reimplement\nthe module from scratch.',
       url='https://github.com/micropython/micropython-lib',
       author='PyPy Developers',
       author_email='pypy-dev@python.org',
-      maintainer='MicroPython Developers',
+      maintainer='micropython-lib Developers',
       maintainer_email='micro-python@googlegroups.com',
       license='MIT',
       cmdclass={'optimize_upip': optimize_upip.OptimizeUpip},
