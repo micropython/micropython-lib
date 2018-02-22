0a581ef3574f2bae45ce4bb6b367bb4a44472685
Paul Sokolovsky <pfalcon@users.sourceforge.net>
2018-02-22 10:38:17 +0200

    uasyncio.udp: Release 0.1.1.

--- uasyncio.udp/setup.py
+++ uasyncio.udp/setup.py
@@ -7,7 +7,7 @@ sys.path.append("..")
 import sdist_upip
 
 setup(name='micropython-uasyncio.udp',
-      version='0.1',
+      version='0.1.1',
       description="UDP support for MicroPython's uasyncio",
       long_description="This is a module reimplemented specifically for MicroPython standard library,\nwith efficient and lean design in mind. Note that this module is likely work\nin progress and likely supports just a subset of CPython's corresponding\nmodule. Please help with the development if you are interested in this\nmodule.",
       url='https://github.com/micropython/micropython-lib',
