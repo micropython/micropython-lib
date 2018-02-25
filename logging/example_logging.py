a4f2d5665d80087fae9a7654148d71aa71723abb
Paul Sokolovsky <pfalcon@users.sourceforge.net>
2018-02-25 10:40:35 +0200

    logging: example_logging: Add testcase for exception().

--- logging/example_logging.py
+++ logging/example_logging.py
@@ -8,3 +8,8 @@ log.warning("Test message3: %d(%s)")
 log.error("Test message4")
 log.critical("Test message5")
 logging.info("Test message6")
+
+try:
+    1/0
+except:
+    log.exception("Some trouble (%s)", "expected")
