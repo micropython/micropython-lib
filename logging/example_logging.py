25e9f1baeacf2285dc3ad35b68b6ef4e4cafaa3d
bogdan-oprea <bogdan.oprea@hpe.com>
2018-05-06 16:20:14 +0300

    logging: moving stress test to examples

--- logging/example_logging.py
+++ logging/example_logging.py
@@ -1,6 +1,38 @@
+import os
+import time
 import logging
+
+
+def reset_log(log_file):
+    try:
+        os.remove(log_file)
+    except OSError:
+        pass
+
+
+def stress_test(iterations):
+    sample = "All work and no play, makes Jack a dull boy!"
+    log_file = 'file.log'
+    reset_log(log_file)
+    logging.basicConfig(filename=log_file)
+    start_time = time.time()
+    for i in range(iterations):
+        logging.info("%d %s" % (i, sample))
+    file_delta = time.time() - start_time
+    reset_log(log_file)
+
+    logging.basicConfig(filename=None)
+    start_time = time.time()
+    for i in range(iterations):
+        logging.info("%d %s" % (i, sample))
+    stdout_delta = time.time() - start_time
+    print("File logging time %f for %i iterations" % (file_delta, iterations))
+    print("Stdout logging time %f for %i iterations" % (stdout_delta, iterations))
+
 
 logging.basicConfig(level=logging.INFO)
 log = logging.getLogger("test")
 log.debug("Test message: %d(%s)", 100, "foobar")
 log.info("Test message2: %d(%s)", 100, "foobar")
+
+#stress_test(100)
--- logging/test_logging.py
+++ logging/test_logging.py
@@ -1,6 +1,5 @@
 import logging
 import os
-import time
 
 
 def reset_log(log_file):
@@ -24,26 +23,6 @@ def test_basicConfig():
 
     print("Success: Testing logging.basicConfig")
 
-
-def stress_test():
-    sample = "All work and no play, makes Jack a dull boy!"
-    log_file = 'file.log'
-    iterations = 100
-    reset_log(log_file)
-    logging.basicConfig(filename=log_file)
-    start_time = time.time()
-    for i in range(iterations):
-        logging.info("%d %s" % (i, sample))
-    file_delta = time.time() - start_time
-    reset_log(log_file)
-
-    logging.basicConfig(filename=None)
-    start_time = time.time()
-    for i in range(iterations):
-        logging.info("%d %s" % (i, sample))
-    stdout_delta = time.time() - start_time
-    print("File logging time %f for %i iterations" % (file_delta, iterations))
-    print("Stdout logging time %f for %i iterations" % (stdout_delta, iterations))
 
 tests = [test_basicConfig]
 
