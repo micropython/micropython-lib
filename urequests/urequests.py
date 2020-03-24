02f59a49069eb542fe17eb4197ebde1b9d04742f
mardigras2020 <60014274+mardigras2020@users.noreply.github.com>
2020-03-24 09:42:28 +0100

    urequests.py: cookies, redirect, url-parameters
    * added optional parameters: params for URL, cookies, followredirect
    * cookie support (necessary for som oauth2 workflows)
    * redirect: follow redirects, store redirect url
    * parameters for URL support added

--- urequests/urequests.py
+++ urequests/urequests.py
@@ -1,4 +1,5 @@
 import usocket
+from parse import urlencode
 
 class Response:
 
@@ -6,6 +7,13 @@ class Response:
         self.raw = f
         self.encoding = "utf-8"
         self._cached = None
+        self.status_code = None
+        self.reason = None
+        self.headers = None
+        # cookie support as dict
+        self.cookies = None
+        # url to see redirect targets
+        self.url = None
 
     def close(self):
         if self.raw:
@@ -31,79 +39,150 @@ class Response:
         import ujson
         return ujson.loads(self.content)
 
+""" method = head, get, put, patch, post, delete
+url (with our without parameters)
+params, cookies, headers - each as dict
+if cookies are supplied, new cookies will be added to the supplied ones. domain attribute is not stored (yet)
+if parse_headers is false -> no cookies are returned as they are part of the header
+if followRedirect = false -> the redirect URL is stored in URL
+"""
+def request(method, url, params=None, cookies=None, data=None, json=None, headers={}, parse_headers=True, followRedirect=True):
+    if params is not None:
+        if params != {}:
+            url = url.rstrip('?') + '?' + urlencode(params, doseq=True)
+    redir_cnt = 1
+    while True:
+        try:
+            proto, dummy, host, path = url.split("/", 3)
+        except ValueError:
+            proto, dummy, host = url.split("/", 2)
+            path = ""
+        if proto == "http:":
+            port = 80
+        elif proto == "https:":
+            import ussl
+            port = 443
+        else:
+            raise ValueError("Unsupported protocol: " + proto)
 
-def request(method, url, data=None, json=None, headers={}, stream=None):
-    try:
-        proto, dummy, host, path = url.split("/", 3)
-    except ValueError:
-        proto, dummy, host = url.split("/", 2)
-        path = ""
-    if proto == "http:":
-        port = 80
-    elif proto == "https:":
-        import ussl
-        port = 443
-    else:
-        raise ValueError("Unsupported protocol: " + proto)
+        if ":" in host:
+            host, port = host.split(":", 1)
+            port = int(port)
 
-    if ":" in host:
-        host, port = host.split(":", 1)
-        port = int(port)
+        ai = usocket.getaddrinfo(host, port, 0, usocket.SOCK_STREAM)
+        ai = ai[0]
 
-    ai = usocket.getaddrinfo(host, port, 0, usocket.SOCK_STREAM)
-    ai = ai[0]
+        resp_d = None
+        if parse_headers is not False:
+            resp_d = {}
 
-    s = usocket.socket(ai[0], ai[1], ai[2])
-    try:
-        s.connect(ai[-1])
-        if proto == "https:":
-            s = ussl.wrap_socket(s, server_hostname=host)
-        s.write(b"%s /%s HTTP/1.0\r\n" % (method, path))
-        if not "Host" in headers:
-            s.write(b"Host: %s\r\n" % host)
-        # Iterate over keys to avoid tuple alloc
-        for k in headers:
-            s.write(k)
-            s.write(b": ")
-            s.write(headers[k])
-            s.write(b"\r\n")
-        if json is not None:
-            assert data is None
-            import ujson
-            data = ujson.dumps(json)
-            s.write(b"Content-Type: application/json\r\n")
-        if data:
-            s.write(b"Content-Length: %d\r\n" % len(data))
-        s.write(b"\r\n")
-        if data:
-            s.write(data)
-
-        l = s.readline()
-        #print(l)
-        l = l.split(None, 2)
-        status = int(l[1])
-        reason = ""
-        if len(l) > 2:
-            reason = l[2].rstrip()
-        while True:
+        # print('Socket create')
+        s = usocket.socket(ai[0], ai[1], ai[2])
+        # 60sec timeout on blocking operations
+        s.settimeout(60.0)
+        try:
+            # print('Socket connect')
+            s.connect(ai[-1])
+            if proto == "https:":
+                s = ussl.wrap_socket(s, server_hostname=host)
+            # print('Socket wrapped')
+            s.write(b"%s /%s HTTP/1.0\r\n" % (method, path))
+            # print('Socket write: ')
+            # print(b"%s /%s HTTP/1.0\r\n" % (method, path))
+            if "Host" not in headers:
+                s.write(b"Host: %s\r\n" % host)
+            # Iterate over keys to avoid tuple alloc
+            for k in headers:
+                s.write(k)
+                s.write(b": ")
+                s.write(headers[k])
+                s.write(b"\r\n")
+                # print(k, b": ".decode('utf-8'), headers[k], b"\r\n".decode('utf-8'))
+            if cookies is not None:
+                for cookie in cookies:
+                    s.write(b"Cookie: ")
+                    s.write(cookie)
+                    s.write(b"=")
+                    s.write(cookies[cookie])
+                    s.write(b"\r\n")
+            if json is not None:
+                assert data is None
+                import ujson
+                data = ujson.dumps(json)
+                s.write(b"Content-Type: application/json\r\n")
+            if data:
+                s.write(b"Content-Length: %d\r\n" % len(data))
+                # print("Content-Length: %d\r\n" % len(data))
+            s.write(b"Connection: close\r\n\r\n")
+            if data:
+                s.write(data)
+                # print(data)
+            # print('Start reading http')
             l = s.readline()
-            if not l or l == b"\r\n":
-                break
-            #print(l)
-            if l.startswith(b"Transfer-Encoding:"):
-                if b"chunked" in l:
-                    raise ValueError("Unsupported " + l)
-            elif l.startswith(b"Location:") and not 200 <= status <= 299:
-                raise NotImplementedError("Redirects not yet supported")
-    except OSError:
-        s.close()
-        raise
-
+            #print('Received protocoll and resultcode %s' % l.decode('utf-8'))
+            l = l.split(None, 2)
+            status = int(l[1])
+            reason = ""
+            if len(l) > 2:
+                reason = l[2].rstrip()
+            # Loop to read header data
+            while True:
+                l = s.readline()
+                #print('Received Headerdata %s' % l.decode('utf-8'))
+                if not l or l == b"\r\n":
+                    break
+                # Header data
+                if l.startswith(b"Transfer-Encoding:"):
+                    if b"chunked" in l:
+                        # decode added, can't cast implicit from bytes to string
+                        raise ValueError("Unsupported " + l.decode('utf-8'))
+                elif l.startswith(b"Location:") and 300 <= status <= 399:
+                    if not redir_cnt:
+                        raise ValueError("Too many redirects")
+                    redir_cnt -= 1
+                    url = l[9:].decode().strip()
+                    #print("Redirect to: %s" % url)
+                    # set status as signal for loop
+                    status = 302
+                if parse_headers is False:
+                    pass
+                elif parse_headers is True:
+                    l = l.decode()
+                    # print('Headers: %s ' % l)
+                    k, v = l.split(":", 1)
+                    # adding cookie support (cookies are overwritten as they have the same key in dict)
+                    # not supported is the domain attribute of cookies, this is not set
+                    # new cookies are added to the supplied cookies
+                    if cookies is None:
+                        cookies = {}
+                    if k == 'Set-Cookie':
+                        ck, cv = v.split("=", 1)
+                        cookies[ck.strip()] = cv.strip()
+                    # else it is not a cookie, just normal header
+                    else:
+                        resp_d[k] = v.strip()
+                else:
+                    parse_headers(l, resp_d)
+        except OSError:
+            s.close()
+            print('Socket closed')
+            raise
+        # if redirect repeat else leave loop
+        if status != 302:
+            break
+        # if redirect false leave loop
+        if (status == 302) and not followRedirect:
+            break
+        # if 302 and redirect = true then loop
     resp = Response(s)
+    resp.url = url
     resp.status_code = status
     resp.reason = reason
+    if resp_d is not None:
+        resp.headers = resp_d
+    # adding cookie support
+    resp.cookies = cookies
     return resp
-
 
 def head(url, **kw):
     return request("HEAD", url, **kw)
