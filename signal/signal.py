d6da964412cd67ed24a80f9613802d516086752f
Hagb Green <hagb_green@qq.com>
2020-10-24 15:41:13 +0800

    signal: Add alarm() and pause() function

--- signal/signal.py
+++ signal/signal.py
@@ -12,6 +12,8 @@ libc = ffilib.libc()
 
 signal_i = libc.func("i", "signal", "ii")
 signal_p = libc.func("i", "signal", "ip")
+alarm_i = libc.func("I", "alarm", "I")
+pause_i = libc.func("i", "pause", "")
 
 def signal(n, handler):
     if isinstance(handler, int):
@@ -19,3 +21,9 @@ def signal(n, handler):
     import ffi
     cb = ffi.callback("v", handler, "i")
     return signal_p(n, cb)
+
+def alarm(n):
+    return alarm_i(n)
+
+def pause():
+    return pause_i()
