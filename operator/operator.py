6f758056ab7c18b2288e1689cd22828dc8d6cab4
John Moser <john.r.moser@gmail.com>
2021-03-26 09:50:11 -0400

    unireedsolomon uses mul, so add some others too

--- operator/operator.py
+++ operator/operator.py
@@ -23,11 +23,43 @@ def eq(a, b):
 def ne(a, b):
     return a != b
 
+def add(a, b):
+    return a + b
+
+def and_(a, b):
+    return a & b
+
+def floordiv(a, b):
+    return a // b
+
+def inv(a):
+    return ~a
+
+invert = inv
+
 def mod(a, b):
     return a % b
 
+def mul(a, b):
+    return a * b
+
+def neg(a):
+    return 0 - a
+
+def or_(a, b):
+    return a | b
+
+def pow(a, b):
+    return a ** b
+
+def sub(a, b):
+    return a - b
+
 def truediv(a, b):
     return a / b
+
+def xor(a, b):
+    return a ^ b
 
 def floordiv(a, b):
     return a // b
