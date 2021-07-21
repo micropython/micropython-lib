13a888d725e03922825a51505d8ea7de27737992
Michael Vogt <mvo@ubuntu.com>
2021-07-21 19:03:10 +0200

    hashlib: import pure python hash first to unbreak the hmac module
    With the current import strategy that favours the uhashlib
    implementation before using the "internal" hashlib version external
    modules like "hmac" break because the uhashlib versions lack properties
    like `{digest,block}_size, copy()`.
    
    This commit changes the import so that the internal version of the
    hash is tried to be imported first and only if there is no internal
    version then the uhashlib version is used.

--- python-stdlib/hashlib/hashlib/__init__.py
+++ python-stdlib/hashlib/hashlib/__init__.py
@@ -6,11 +6,18 @@ except ImportError:
 
 def init():
     for i in ("sha1", "sha224", "sha256", "sha384", "sha512"):
-        c = getattr(uhashlib, i, None)
-        if not c:
+        # first try to import the python-stdlib hash so that we are compatible
+        # with e.g. hmac (uhashlib lacks {digest,block}_size, copy())
+        try:
             c = __import__("_" + i, None, None, (), 1)
             c = getattr(c, i)
-        globals()[i] = c
+        except ImportError:
+            c = None
+        # fallback to uhashlib (e.g. sha1)
+        if not c:
+            c = getattr(uhashlib, i, None)
+        if c:
+            globals()[i] = c
 
 
 init()
