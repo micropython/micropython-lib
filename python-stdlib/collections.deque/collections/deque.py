f3acedb207df8a6709852bece70776826f57e7eb
Matt Trentini <matt.trentini@gmail.com>
2021-08-14 01:13:11 +1000

    collections.deque: Add maxlen support and remove popright (non-standard).

--- python-stdlib/collections.deque/collections/deque.py
+++ python-stdlib/collections.deque/collections/deque.py
@@ -1,27 +1,40 @@
 class deque:
-    def __init__(self, iterable=None):
+    def __init__(self, iterable=None, maxlen=None):
         if iterable is None:
             self.q = []
         else:
             self.q = list(iterable)
+        if maxlen is not None:
+            if not isinstance(maxlen, int):
+                raise TypeError("" "an integer is required")
+            if maxlen < 0:
+                raise ValueError("" "maxlen must be non-negative")
+        self.__maxlen = maxlen
 
     def popleft(self):
         return self.q.pop(0)
-
-    def popright(self):
-        return self.q.pop()
 
     def pop(self):
         return self.q.pop()
 
     def append(self, a):
         self.q.append(a)
+        if self.__maxlen is not None and len(self.q) > self.__maxlen:
+            self.popleft()
 
     def appendleft(self, a):
         self.q.insert(0, a)
+        if self.__maxlen is not None and len(self.q) > self.__maxlen:
+            self.pop()
 
     def extend(self, a):
+        if len(self.q) + len(a) > self.__maxlen:
+            raise IndexError
         self.q.extend(a)
+
+    @property
+    def maxlen(self):
+        return self.__maxlen
 
     def __len__(self):
         return len(self.q)
