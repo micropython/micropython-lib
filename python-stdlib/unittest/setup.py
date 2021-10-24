db4c739863e49fc874bdaae8aa8c316c7ed4276a
Andrew Leech <andrew.leech@planetinnovation.com.au>
2021-10-24 09:17:03 +0300

    unittest: Version 0.9.0

--- python-stdlib/unittest/setup.py
+++ python-stdlib/unittest/setup.py
@@ -10,7 +10,7 @@ import sdist_upip
 
 setup(
     name="micropython-unittest",
-    version="0.3.2",
+    version="0.9.0",
     description="unittest module for MicroPython",
     long_description="This is a module reimplemented specifically for MicroPython standard library,\nwith efficient and lean design in mind. Note that this module is likely work\nin progress and likely supports just a subset of CPython's corresponding\nmodule. Please help with the development if you are interested in this\nmodule.",
     url="https://github.com/micropython/micropython-lib",
