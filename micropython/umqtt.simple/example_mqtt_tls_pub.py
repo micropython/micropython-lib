d035d9e2f7b896aea1f2d733cba613c78a078a25
Mr Keuz <mrkeuz@users.noreply.github.com>
2021-10-29 17:16:24 +0300

    Add example for mqtt tls

---
+++ micropython/umqtt.simple/example_mqtt_tls_pub.py
@@ -0,0 +1,20 @@
+from umqtt.simple import MQTTClient
+
+# Test reception e.g. with:
+# mosquitto_sub -t foo_topic
+
+def main(server="localhost"):
+    # NOTE: Checking server certificate is DISABLE
+    ssl_params = {"server_hostname": server}
+    c = MQTTClient("umqtt_client",
+                   server=server,
+                   port=8883,
+                   user="<username>",
+                   password="<password>",
+                   keepalive=60,
+                   ssl=True,
+                   ssl_params=ssl_params)
+
+
+if __name__ == "__main__":
+    main()
