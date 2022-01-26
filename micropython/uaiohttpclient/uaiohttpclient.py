c7e5d75cb2f5f03b3016440de8315b826c5d85bf
Eli Clement <eli.clement@entstudios.com>
2022-01-26 14:36:07 +0100

    use variable port instead of hard coded port 80

--- micropython/uaiohttpclient/uaiohttpclient.py
+++ micropython/uaiohttpclient/uaiohttpclient.py
@@ -53,7 +53,7 @@ def request_raw(method, url):
         port = 443
     else:
         raise ValueError("Unsupported protocol: " + proto)
-    reader, writer = yield from asyncio.open_connection(host, 80)
+    reader, writer = yield from asyncio.open_connection(host, port)
     # Use protocol 1.0, because 1.1 always allows to use chunked transfer-encoding
     # But explicitly set Connection: close, even though this should be default for 1.0,
     # because some servers misbehave w/o it.
