70e422dc2e885bbaafe6eb7e3d81118e17d4b555
Andrew Leech <andrew.leech@planetinnovation.com.au>
2022-06-20 13:23:13 +1000

    urequests: Release 0.7.0.

--- python-ecosys/urequests/setup.py
+++ python-ecosys/urequests/setup.py
@@ -10,11 +10,11 @@ import sdist_upip
 
 setup(
     name="micropython-urequests",
-    version="0.6",
+    version="0.7",
     description="urequests module for MicroPython",
     long_description="This is a module reimplemented specifically for MicroPython standard library,\nwith efficient and lean design in mind. Note that this module is likely work\nin progress and likely supports just a subset of CPython's corresponding\nmodule. Please help with the development if you are interested in this\nmodule.",
     url="https://github.com/micropython/micropython-lib",
-    author="Paul Sokolovsky",
+    author="micropython-lib Developers",
     author_email="micro-python@googlegroups.com",
     maintainer="micropython-lib Developers",
     maintainer_email="micro-python@googlegroups.com",
