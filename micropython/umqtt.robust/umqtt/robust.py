286f22a2d655a788e3b9e5d17466e19f69d9c667
Ian Cotter-Llewellyn <ian_llewellyn@hotmail.com>
2022-07-19 01:37:08 +0100

    fixup! umqtt.robust: Fix check_msg blocking after reconnect

--- micropython/umqtt.robust/umqtt/robust.py
+++ micropython/umqtt.robust/umqtt/robust.py
@@ -36,11 +36,18 @@ class MQTTClient(simple.MQTTClient):
             self.reconnect()
 
     def wait_msg(self):
-        timeout_ms = float(str(self.sock).split(' ')[2].split('=')[1])
         while 1:
             try:
                 return super().wait_msg()
             except OSError as e:
                 self.log(False, e)
             self.reconnect()
-            self.sock.settimeout(timeout_ms / 1000 if timeout >= 0 else None)
+
+    def check_msg(self):
+        while 1:
+            self.sock.setblocking(False)
+            try:
+                return super().wait_msg()
+            except OSError as e:
+                self.log(False, e)
+            self.reconnect()
