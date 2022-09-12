f1039fd2f27c6d444e41ec79acabe4715cb76eb7
Jim Mussared <jim.mussared@gmail.com>
2022-09-12 14:38:52 +1000

    collections: Rename collections.* to collections-*.
    This is to match the convention for naming "extension" packages.
    
    Signed-off-by: Jim Mussared <jim.mussared@gmail.com>

--- python-stdlib/collections-defaultdict/manifest.py
+++ python-stdlib/collections-defaultdict/manifest.py
@@ -2,4 +2,5 @@ metadata(version="0.3")
 
 # Originally written by Paul Sokolovsky.
 
+require("collections")
 package("collections")
--- python-stdlib/collections-deque/manifest.py
+++ python-stdlib/collections-deque/manifest.py
@@ -1,3 +1,4 @@
 metadata(version="0.1.3")
 
+require("collections")
 package("collections")
