d8a8be7a96d7c6e32af2c1c20cea8863755c2dfe
jef <jef@slechte.info>
2022-12-20 23:56:45 +0100

    umqtt.robust: When reconnecting, close the previous socket.

--- micropython/umqtt.robust/umqtt/robust.py
+++ micropython/umqtt.robust/umqtt/robust.py
@@ -21,6 +21,11 @@ class MQTTClient(simple.MQTTClient):
         i = 0
         while 1:
             try:
+                if self.sock:
+                    self.sock.close()
+            except OSError as e:
+                self.log(True, e)
+            try:
                 return super().connect(False)
             except OSError as e:
                 self.log(True, e)
