769926b3f9e19cc9427889f1c5f7fad6d851b589
Andrzej Kowalczyk <x0@06f65b198d.me>
2023-01-30 11:11:42 +0100

    Unittest mod; Fix AttributeError when unittests tries to handle exceptions; Fix traceback module shadowing by local variables in _capture_exc func;

--- python-stdlib/unittest/unittest/__init__.py
+++ python-stdlib/unittest/unittest/__init__.py
@@ -332,12 +332,12 @@ class TestResult:
         return self
 
 
-def _capture_exc(exc, traceback):
+def _capture_exc(exc, exc_traceback):
     buf = io.StringIO()
     if hasattr(sys, "print_exception"):
         sys.print_exception(exc, buf)
     elif traceback is not None:
-        traceback.print_exception(None, exc, traceback, file=buf)
+        traceback.print_exception(None, exc, exc_traceback, file=buf)
     return buf.getvalue()
 
 
@@ -401,12 +401,10 @@ def _run_suite(c, test_result: TestResult, suite_name=""):
             test_result.skippedNum += 1
             test_result.skipped.append((name, c, reason))
         except Exception as ex:
-            if hasattr(sys, 'exc_info'):
-                exc_info = sys.exc_info()
-            else:
-                exc_info = (ex.__class__, ex, None)
             _handle_test_exception(
-                current_test=(name, c), test_result=test_result, exc_info=exc_info
+                current_test=(name, c),
+                test_result=test_result,
+                exc_info=(type(exc), exc, None)
             )
             # Uncomment to investigate failure in detail
             # raise
