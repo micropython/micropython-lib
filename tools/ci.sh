c1526d2d1eb68c4c3b0ff8940b012c98a80301f1
Damien George <damien@micropython.org>
2023-02-01 11:48:37 +1100

    github/workflows: Add workflow to build all packages.
    Signed-off-by: Damien George <damien@micropython.org>

--- tools/ci.sh
+++ tools/ci.sh
@@ -14,3 +14,29 @@ function ci_code_formatting_setup {
 function ci_code_formatting_run {
     tools/codeformat.py -v
 }
+
+########################################################################################
+# build packages
+
+function ci_build_packages_setup {
+    git clone https://github.com/micropython/micropython.git /tmp/micropython
+
+    # build mpy-cross (use -O0 to speed up the build)
+    make -C /tmp/micropython/mpy-cross -j CFLAGS_EXTRA=-O0
+
+    # check the required programs run
+    /tmp/micropython/mpy-cross/build/mpy-cross --version
+    python3 /tmp/micropython/tools/manifestfile.py --help
+}
+
+function ci_build_packages_check_manifest {
+    for file in $(find -name manifest.py); do
+        echo "##################################################"
+        echo "# Testing $file"
+        python3 /tmp/micropython/tools/manifestfile.py --lib . --compile $file
+    done
+}
+
+function ci_build_packages_compile_index {
+    python3 tools/build.py --micropython /tmp/micropython --output /tmp/micropython-lib-deploy
+}
