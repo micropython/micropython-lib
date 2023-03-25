8b35e1d59a81e80dd0c6b4f149941e3552814b8a
Jonas Scharpf <jonas@brainelectronics.de>
2023-03-25 15:05:30 +0100

    upip: add missing required package, fix code format

--- micropython/upip/manifest.py
+++ micropython/upip/manifest.py
@@ -1,6 +1,9 @@
-metadata(version="0.1.0", description="On-device PyPI package installer for network-capable boards")
+metadata(
+    version="0.1.0", description="On-device PyPI package installer for network-capable boards"
+)
 
 require("shutil")
 require("utarfile")
+require("urequests")
 
 package("upip", opt=3)
