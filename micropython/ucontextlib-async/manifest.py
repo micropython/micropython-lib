ad857b32d54efd5cd79b6f9036149cf7b493954f
Andrew Leech <andrew.leech@planetinnovation.com.au>
2023-05-09 12:59:26 +1000

    micropython/ucontextlib-async: Add asynccontextmanager in extension package.

---
+++ micropython/ucontextlib-async/manifest.py
@@ -0,0 +1,4 @@
+metadata(description="asynccontextmanager support in ucontextlib", version="0.1.1")
+
+require("ucontextlib")
+package("ucontextlib")
--- micropython/ucontextlib/manifest.py
+++ micropython/ucontextlib/manifest.py
@@ -2,4 +2,4 @@ metadata(
     description="Minimal subset of contextlib for MicroPython low-memory ports", version="0.1.1"
 )
 
-module("ucontextlib.py")
+package("ucontextlib")
