de9a0529bf43adc822666b1cbf95df9a0c1008fe
Ned Konz <ned@productcreationstudio.com>
2023-06-10 14:42:00 -0700

    mip: Support relative source file paths in package.json.
    This adds the ability for `mip` to load files from _relative_ source
    paths given in the `urls` section of `package.json` files.
    
    Previously, the only accepted forms for source paths were
    "http://", "https://", or "github:".
    
    These changes allow a source path to be given without one of these prefixes,
    in which case the URL of the parent of the `package.json` is prepended.
    
    So instead of someone forking a repository and having to change source lines
    that start with
        github:upstream_repository/filename
    into
        github:my_repository/filename
    they can now just say
        filename
    
    which will survive forking or cloning without change.
    
    This also allows doing `mip.install()` from a `file://` location.
    This makes it easy to do a `git clone` of a repository and test
    installation of a package from the local filesystem, especially
    using the unix port.
    
    Signed-off-by: Ned Konz <ned@productcreationstudio.com>

--- micropython/mip/mip/__init__.py
+++ micropython/mip/mip/__init__.py
@@ -1,13 +1,17 @@
 # MicroPython package installer
 # MIT license; Copyright (c) 2022 Jim Mussared
+# Modified by Ned Konz to allow relative source URLs in package.json files
 
 import urequests as requests
 import sys
 
-
 _PACKAGE_INDEX = const("https://micropython.org/pi/v2")
 _CHUNK_SIZE = 128
+_URL_PREFIXES = const(("http://", "https://", "github:", "file://"))
 
+# Return true if name is a URI that we understand
+def _is_url(name):
+    return any(name.startswith(prefix) for prefix in _URL_PREFIXES)
 
 # This implements os.makedirs(os.dirname(path))
 def _ensure_path_exists(path):
@@ -57,10 +61,16 @@ def _check_exists(path, short_hash):
         return False
 
 
-def _rewrite_url(url, branch=None):
-    if not branch:
-        branch = "HEAD"
+def _rewrite_url(url, branch=None, package_json_url=None):
+    # rewrite relative URLs as absolute URLs
+    if not _is_url(url):
+        if not package_json_url:
+            raise ValueError("No package.json URL specified")
+        url = package_json_url.rsplit("/", 1)[0] + "/" + url
+    # now rewrite github: URLs as raw.githubusercontent.com URLs
     if url.startswith("github:"):
+        if not branch:
+            branch = "HEAD"
         url = url[7:].split("/")
         url = (
             "https://raw.githubusercontent.com/"
@@ -76,6 +86,20 @@ def _rewrite_url(url, branch=None):
 
 
 def _download_file(url, dest):
+    # if url is a file:// url, just copy it
+    if url.startswith("file://"):
+        src_name = url[7:]
+        try:
+            with open(src_name, "rb") as src:
+                print(f"Copying file {src_name} to {dest}")
+                _ensure_path_exists(dest)
+                with open(dest, "wb") as dst:
+                    _chunk(src, dst.write)
+            return True
+        except OSError:
+            print(f"File {src_name} not found")
+            return False
+
     response = requests.get(url)
     try:
         if response.status_code != 200:
@@ -93,15 +117,27 @@ def _download_file(url, dest):
 
 
 def _install_json(package_json_url, index, target, version, mpy):
-    response = requests.get(_rewrite_url(package_json_url, version))
-    try:
-        if response.status_code != 200:
-            print("Package not found:", package_json_url)
+    # if package_json_url is a file:// url, just download it
+    # and use its json directly
+    if package_json_url.startswith("file://"):
+        import ujson as json
+        pkg_file_name = package_json_url[7:]
+        try:
+            with open(pkg_file_name) as json_file:
+                package_json = json.load(json_file)
+        except OSError:
+            print(f"File {pkg_file_name} not found")
             return False
+    else:
+        response = requests.get(_rewrite_url(package_json_url, version))
+        try:
+            if response.status_code != 200:
+                print("Package not found:", package_json_url)
+                return False
 
-        package_json = response.json()
-    finally:
-        response.close()
+            package_json = response.json()
+        finally:
+            response.close()
     for target_path, short_hash in package_json.get("hashes", ()):
         fs_target_path = target + "/" + target_path
         if _check_exists(fs_target_path, short_hash):
@@ -113,7 +149,7 @@ def _install_json(package_json_url, index, target, version, mpy):
                 return False
     for target_path, url in package_json.get("urls", ()):
         fs_target_path = target + "/" + target_path
-        if not _download_file(_rewrite_url(url, version), fs_target_path):
+        if not _download_file(_rewrite_url(url, version, package_json_url), fs_target_path):
             print("File not found: {} {}".format(target_path, url))
             return False
     for dep, dep_version in package_json.get("deps", ()):
@@ -123,11 +159,7 @@ def _install_json(package_json_url, index, target, version, mpy):
 
 
 def _install_package(package, index, target, version, mpy):
-    if (
-        package.startswith("http://")
-        or package.startswith("https://")
-        or package.startswith("github:")
-    ):
+    if _is_url(package):
         if package.endswith(".py") or package.endswith(".mpy"):
             print("Downloading {} to {}".format(package, target))
             return _download_file(
