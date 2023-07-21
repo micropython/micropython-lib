87b4cdae2fdb0f7975c823f471baa65951fae3ce
Jim Mussared <jim.mussared@gmail.com>
2023-07-21 21:24:36 +1000

    aiorepl: Bump patch version.
    For changes in ff842310de5a3c9ea5a5aa5cbaa52e06e5f2952b.
    
    Signed-off-by: Jim Mussared <jim.mussared@gmail.com>

--- micropython/aiorepl/manifest.py
+++ micropython/aiorepl/manifest.py
@@ -1,5 +1,5 @@
 metadata(
-    version="0.1",
+    version="0.1.1",
     description="Provides an asynchronous REPL that can run concurrently with an asyncio, also allowing await expressions.",
 )
 
