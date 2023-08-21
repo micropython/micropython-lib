82501d721f7cbcc04cd89521617ebc374f1ac1c1
Andrew Leech <andrew.leech@planetinnovation.com.au>
2023-08-21 12:08:30 +0930

    stdlib/os: Update method of extending builtin os.

--- python-stdlib/os/os/__init__.py
+++ python-stdlib/os/os/__init__.py
@@ -1,5 +1,11 @@
-# Replace built-in os module.
-from uos import *
+# Include built-in os module.
+import sys
+__path = sys.path
+try:
+    sys.path.clear()
+    from os import *
+finally:
+    sys.path.extend(__path)
 
 # Provide optional dependencies (which may be installed separately).
 try:
