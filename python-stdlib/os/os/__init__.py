f4cfc3c71a5e5ba56df586f8aecb833d8ab51269
Andrew Leech <andrew.leech@planetinnovation.com.au>
2023-08-21 12:13:56 +0930

    os: Provide namedtuple response for os.stat().
    Having the option to wrap / return os.stat() with the cpython standard
    format can greatly aid in porting third party libraries.

--- python-stdlib/os/os/__init__.py
+++ python-stdlib/os/os/__init__.py
@@ -6,3 +6,28 @@ try:
     from . import path
 except ImportError:
     pass
+
+from collections import namedtuple
+
+# https://docs.python.org/3/library/os.html#os.stat_result
+stat_result = namedtuple(
+    "stat_result",
+    (
+        "st_mode",
+        "st_ino",
+        "st_dev",
+        "st_nlink",
+        "st_uid",
+        "st_gid",
+        "st_size",
+        "st_atime",
+        "st_mtime",
+        "st_ctime",
+    ),
+)
+
+__os_stat = stat
+
+
+def stat(path):
+    return stat_result(*__os_stat(path))
