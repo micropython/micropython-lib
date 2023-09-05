13c89fc5ef7ec057b42feac0e117b45855c9b1ff
Carlosgg <carlosgilglez@gmail.com>
2023-09-05 03:56:54 +0100

    micropython/uaiohttpclient: Add ssl support.
    Add ssl support and SSLContext support with ClientSession
    
    Signed-off-by: Carlos Gil <carlosgilglez@gmail.com>

---
+++ micropython/uaiohttpclient/clientsession_example.py
@@ -0,0 +1,17 @@
+import uaiohttpclient as aiohttp
+import asyncio
+
+
+async def fetch(client):
+    async with client.get("http://micropython.org") as resp:
+        assert resp.status == 200
+        return await resp.text()
+
+
+async def main():
+    async with aiohttp.ClientSession() as client:
+        html = await fetch(client)
+        print(html)
+
+
+asyncio.run(main())
