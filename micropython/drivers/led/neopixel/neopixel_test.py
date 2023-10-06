1f1f51d57c8e171eddbecf85ac01d96b4429d079
Tom Mount <tmountjr@gmail.com>
2023-10-06 12:32:54 -0400

    drivers/led/neopixel: Fix test formatting.
    Signed-off-by: Tom Mount <tmountjr@gmail.com>

--- micropython/drivers/led/neopixel/neopixel_test.py
+++ micropython/drivers/led/neopixel/neopixel_test.py
@@ -7,20 +7,23 @@ def neopixel_test():
     print("Fill with a color.")
     np.fill((255, 128, 64))
     print("Verify the bytes to be written")
-    expected_bytearray = bytearray([255, 128, 64, 255, 128, 64, 255, 128, 64])
-    actual_bytearray = np.buf
-    print(f'Initial fill: {"passed" if expected_bytearray == actual_bytearray else "failed"}.')
+    expected = bytearray([255, 128, 64, 255, 128, 64, 255, 128, 64])
+    actual = np.buf
+    passed = "passed" if expected == actual else "failed"
+    print(f"Initial fill: {passed}.")
     print()
 
     print("Change brightness of all pixels.")
     np.brightness(0.5)
-    expected_bytearray = bytearray([127, 64, 32, 127, 64, 32, 127, 64, 32])
-    actual_bytearray = np.buf
-    print(f'Brightness change: {"passed" if expected_bytearray == actual_bytearray else "failed"}.')
+    expected = bytearray([127, 64, 32, 127, 64, 32, 127, 64, 32])
+    actual = np.buf
+    passed = "passed" if expected == actual else "failed"
+    print(f"Brightness change: {passed}.")
     print()
 
     print("Get current brightness.")
-    expected_brightness = 0.5
-    actual_brightness = np.brightness()
-    print(f'Brightness get: {"passed" if expected_brightness == actual_brightness else "failed"}.')
+    expected = 0.5
+    actual = np.brightness()
+    passed = "passed" if expected == actual else "failed"
+    print(f"Brightness get: {passed}.")
     print()
