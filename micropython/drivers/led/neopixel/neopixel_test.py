6ca933b9f21da5ffcecd06a5a812a0c940a3d5f4
Tom Mount <tmountjr@gmail.com>
2023-10-06 12:27:28 -0400

    drivers/led/neopixel: Fix formatting.
    Signed-off-by: Tom Mount <tmountjr@gmail.com>

--- micropython/drivers/led/neopixel/neopixel_test.py
+++ micropython/drivers/led/neopixel/neopixel_test.py
@@ -9,24 +9,18 @@ def neopixel_test():
     print("Verify the bytes to be written")
     expected_bytearray = bytearray([255, 128, 64, 255, 128, 64, 255, 128, 64])
     actual_bytearray = np.buf
-    print(
-        f'Initial fill: {"passed" if expected_bytearray == actual_bytearray else "failed"}.'
-    )
+    print(f'Initial fill: {"passed" if expected_bytearray == actual_bytearray else "failed"}.')
     print()
 
     print("Change brightness of all pixels.")
     np.brightness(0.5)
     expected_bytearray = bytearray([127, 64, 32, 127, 64, 32, 127, 64, 32])
     actual_bytearray = np.buf
-    print(
-        f'Brightness change: {"passed" if expected_bytearray == actual_bytearray else "failed"}.'
-    )
+    print(f'Brightness change: {"passed" if expected_bytearray == actual_bytearray else "failed"}.')
     print()
 
     print("Get current brightness.")
     expected_brightness = 0.5
     actual_brightness = np.brightness()
-    print(
-        f'Brightness get: {"passed" if expected_brightness == actual_brightness else "failed"}.'
-    )
+    print(f'Brightness get: {"passed" if expected_brightness == actual_brightness else "failed"}.')
     print()
