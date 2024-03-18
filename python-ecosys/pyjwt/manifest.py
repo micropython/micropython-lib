4e43bc0c78ba368e81490b75bb84ed395085970f
Jonah Bron <hi@jonah.id>
2024-03-18 05:32:31 +0000

    pyjwt: Incremented package version and added a brief description.
    Signed-off-by: Jonah Bron <hi@jonah.id>

--- python-ecosys/pyjwt/manifest.py
+++ python-ecosys/pyjwt/manifest.py
@@ -1,4 +1,13 @@
-metadata(version="0.1.0", pypi="pyjwt")
+metadata(
+    version="0.2.0",
+    pypi="pyjwt",
+    description="""
+JWT library for MicroPython.  Supports HMAC (HS256) encoding essentially.
+Optionally supports ECDSA (ES256) asymmetric-key signing/verification when the
+[dmazella/ucryptography](https://github.com/dmazzella/ucryptography/) library
+is available in the MicroPython firmware.
+""",
+)
 
 require("hmac")
 
