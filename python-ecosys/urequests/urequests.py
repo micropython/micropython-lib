2c2284bda573f53053655c4d3937e4a79b074a4c
Brendon Duncan <b.duncan@uqconnect.edu.au>
2024-07-09 13:33:05 +1000

    Update urequests.py

--- python-ecosys/urequests/urequests.py
+++ python-ecosys/urequests/urequests.py
@@ -1,5 +1,6 @@
 import usocket
-
+import ujson
+import ussl
 
 class Response:
     def __init__(self, f):
@@ -28,8 +29,6 @@ class Response:
         return str(self.content, self.encoding)
 
     def json(self):
-        import ujson
-
         return ujson.loads(self.content)
 
 
@@ -63,8 +62,6 @@ def request(
     if proto == "http:":
         port = 80
     elif proto == "https:":
-        import ussl
-
         port = 443
     else:
         raise ValueError("Unsupported protocol: " + proto)
@@ -102,7 +99,6 @@ def request(
             s.write(b"\r\n")
         if json is not None:
             assert data is None
-            import ujson
 
             data = ujson.dumps(json)
             s.write(b"Content-Type: application/json\r\n")
