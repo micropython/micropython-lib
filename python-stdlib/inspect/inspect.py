b251c59be11da8a1c456db78a4f6c96349dfb76b
Damien George <damien@micropython.org>
2025-04-11 12:23:33 +1000

    inspect: Fix isgenerator logic.
    Also optimise both `isgenerator()` and `isgeneratorfunction()` so they use
    the same lambda, and don't have to create it each time they are called.
    
    Fixes issue #997.
    
    Signed-off-by: Damien George <damien@micropython.org>

--- python-stdlib/inspect/inspect.py
+++ python-stdlib/inspect/inspect.py
@@ -1,5 +1,7 @@
 import sys
 
+_g = lambda: (yield)
+
 
 def getmembers(obj, pred=None):
     res = []
@@ -16,11 +18,11 @@ def isfunction(obj):
 
 
 def isgeneratorfunction(obj):
-    return isinstance(obj, type(lambda: (yield)))
+    return isinstance(obj, type(_g))
 
 
 def isgenerator(obj):
-    return isinstance(obj, type(lambda: (yield)()))
+    return isinstance(obj, type((_g)()))
 
 
 class _Class:
