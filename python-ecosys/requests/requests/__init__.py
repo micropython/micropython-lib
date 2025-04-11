1e68c60442951b013eaec0dd492deb14aa1cd7ab
黃昕暐 <meebox@gmail.com>
2025-04-11 13:41:24 +0800

    pop the  item "Host" in the efficient way
    Use `headers.pop("Host", None)` instead of a `if` statement.
    
    Signed-off-by: 黃昕暐 <meebox@gmail.com>

--- python-ecosys/requests/requests/__init__.py
+++ python-ecosys/requests/requests/__init__.py
@@ -181,8 +181,7 @@ def request(
     if redirect:
         s.close()
         # use the Host in the redirect URL
-        if "Host" in headers:
-            headers.pop("Host")
+        headers.pop("Host", None)
         if status in [301, 302, 303]:
             return request("GET", redirect, None, None, headers, stream)
         else:
