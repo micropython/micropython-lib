913498ef05a5b9c28a246d8db64e31f7c12a35e8
Andrew Leech <andrew.leech@planetinnovation.com.au>
2025-05-07 21:19:43 +1000

    aioble-l2cap: Raise correct error if l2cap disconnects during send.
    If BLE disconnects in the middle of a send process, this change makes the
    code raise `L2CAPDisconnectedError` rather than `TypeError` due to `_cid`
    being `None`.
    
    Signed-off-by: Andrew Leech <andrew.leech@planetinnovation.com.au>

--- micropython/bluetooth/aioble/manifest.py
+++ micropython/bluetooth/aioble/manifest.py
@@ -3,7 +3,7 @@
 # code. This allows (for development purposes) all the files to live in the
 # one directory.
 
-metadata(version="0.6.0")
+metadata(version="0.6.1")
 
 # Default installation gives you everything. Install the individual
 # components (or a combination of them) if you want a more minimal install.
