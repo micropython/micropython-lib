e5b2b138b41c3f5307b625f17eeea7b8248dcf23
Damien George <damien@micropython.org>
2025-12-02 13:07:34 +1100

    tarfile: Add basic unittest for tarfile.TarFile.
    Tests TarFile iteration and extracting file information.
    
    Signed-off-by: Damien George <damien@micropython.org>

---
+++ python-stdlib/tarfile/test_tarfile.py
@@ -0,0 +1,37 @@
+import tarfile
+import unittest
+
+
+test_tar_contents = (
+    ("a", "file", 2),
+    ("b", "file", 2),
+    ("dir/", "dir", 0),
+    ("dir/c", "file", 2),
+    ("dir/d", "file", 2),
+    ("tar.tar", "file", 10240),
+)
+
+test_sub_tar_contents = (
+    ("e", "file", 2),
+    ("f", "file", 2),
+)
+
+
+class TestTarFile(unittest.TestCase):
+    def check_contents(self, expected, tf):
+        for i, file in enumerate(tf):
+            name, type, size = expected[i]
+            self.assertEqual(file.name, name)
+            self.assertEqual(file.type, type)
+            self.assertEqual(file.size, size)
+
+    def test_iter(self):
+        tf = tarfile.TarFile("test.tar")
+        for _ in range(6):
+            self.assertIsInstance(next(tf), tarfile.TarInfo)
+        with self.assertRaises(StopIteration):
+            next(tf)
+
+    def test_contents(self):
+        tf = tarfile.TarFile("test.tar")
+        self.check_contents(test_tar_contents, tf)
