09a33ee93fd20bfbb92c3338f4a4849f16348e09
Daniel Houck <Software@DRHouck.me>
2026-02-18 00:12:22 -0500

    unix-ffi/json: Fix json.loads() of a bytes object.
    The conversion from bytes to string previosuly only if some other
    argument was also given to `loads`.  This makes even a basic call with
    no other arguments work.
    
    Signed-off-by: Daniel Houck <Software@DRHouck.me>

--- unix-ffi/json/test_json.py
+++ unix-ffi/json/test_json.py
@@ -11,3 +11,9 @@ print(outp)
 
 # Doesn't work because JSON doesn't have tuples
 # assert inp == outp
+
+b = b'["foo", {"bar": ["baz", null, 1, 2]}]'
+outp2 = json.loads(b)
+
+assert b.decode('utf-8') == s
+assert outp == outp2
