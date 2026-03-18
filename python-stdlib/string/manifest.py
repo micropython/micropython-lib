8380c7bb8f9e5e5260e9539156742925e00366b2
Damien George <damien@micropython.org>
2026-03-18 17:14:16 +1100

    string: Convert string module to package and import templatelib.
    Convert the existing `string` module into a package with `__init__.py`
    and `templatelib.py`.  This is needed to support t-strings (if they are
    enabled) because there is a built-in `string.templatelib` module with
    built-in `Template` and `Interpolation` classes.
    
    Signed-off-by: Damien George <damien@micropython.org>

--- python-stdlib/string/manifest.py
+++ python-stdlib/string/manifest.py
@@ -1,3 +1,3 @@
-metadata(version="0.1.1")
+metadata(version="0.2.0")
 
-module("string.py")
+package("string")
---
+++ python-stdlib/string/string/templatelib.py
@@ -0,0 +1,5 @@
+# Import built-in t-string classes if they exist.
+try:
+    from ustring.templatelib import *
+except:
+    pass
