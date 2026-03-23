1985bdbb2693ef1064e50de6799ab8cc9db65405
Alessandro Gatti <a.gatti@frob.it>
2026-03-23 11:06:21 +0100

    python-stdlib/collections-defaultdict: Add items() function.
    This commit adds the `items()` functions to `collections.defaultdict`,
    acting as a simple proxy to the underlying dictionary container.
    
    This fixes #350.
    
    Signed-off-by: Alessandro Gatti <a.gatti@frob.it>

--- python-stdlib/collections-defaultdict/test_defaultdict.py
+++ python-stdlib/collections-defaultdict/test_defaultdict.py
@@ -8,3 +8,11 @@ del d[1]
 assert d[1] == 42
 
 assert "foo" not in d
+
+d = defaultdict.defaultdict(list)
+for k, v in [('a', 1), ('b', 2), ('c', 3), ('a', 4), ('b', 5), ('c', 6)]:
+    d[k].append(v)
+i = list(sorted(d.items()))
+assert i[0] == ('a', [1, 4])
+assert i[1] == ('b', [2, 5])
+assert i[2] == ('c', [3, 6])
