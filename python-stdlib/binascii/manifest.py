60a055daf75c68b9bc9ab3e5d613683eeef3703b
Alessandro Gatti <a.gatti@frob.it>
2026-09-06 04:54:08 +0200

    binascii: Reduce module RAM overhead and compiled footprint.
    This commit makes some changes to the `binascii` module to reduce its
    footpint once compiled, and marginally improve the time it takes to
    import the module itself.
    
    The decoding table is now precomputed as a 256 entries long `bytes`
    object rather than doing the table building at import time.  The table
    was built as a list of integers, so there's some considerable overhead
    compared to a single `bytes` object.
    
    Decoding data would also do some unnecessary roundtrips between
    integers and characters for the table lookup to figure out the 6-bits
    sequence to use for a given character.  The original code, despite
    having a function called "a2b_base64" does not work with strings, so
    the roundtrips were not needed to begin with.  CPython's equivalent
    works with strings too, but the changes proposed here do not change
    the function's behaviour.
    
    There was some dead code in the encoder function, used to calculate the
    final size of the output buffer.  The code uses a list anyway, so this
    is not needed.
    
    The tables are now built using `const()`, and made private (hence the
    minor version number being bumped up).  This saves even more space when
    compiled.
    
    Finally, the base64 part of the test suite was updated a bit to cover
    more padding cases.
    
    These changes reduce the compiled module size by 204 bytes.
    
    Signed-off-by: Alessandro Gatti <a.gatti@frob.it>

--- python-stdlib/binascii/manifest.py
+++ python-stdlib/binascii/manifest.py
@@ -1,3 +1,3 @@
-metadata(version="2.4.1")
+metadata(version="2.5.0")
 
 module("binascii.py")
--- python-stdlib/binascii/test_binascii.py
+++ python-stdlib/binascii/test_binascii.py
@@ -12,7 +12,45 @@ data2 = unhexlify(h)
 if data2 != data:
     raise Exception("Error")
 
-a2b_base64(b"as==") == b"j"
+s = b"Hello from MicroPython!\n\xff\x80\xa0\xcf"
+b64 = (
+    b"\n",
+    b"SA==\n",
+    b"SGU=\n",
+    b"SGVs\n",
+    b"SGVsbA==\n",
+    b"SGVsbG8=\n",
+    b"SGVsbG8g\n",
+    b"SGVsbG8gZg==\n",
+    b"SGVsbG8gZnI=\n",
+    b"SGVsbG8gZnJv\n",
+    b"SGVsbG8gZnJvbQ==\n",
+    b"SGVsbG8gZnJvbSA=\n",
+    b"SGVsbG8gZnJvbSBN\n",
+    b"SGVsbG8gZnJvbSBNaQ==\n",
+    b"SGVsbG8gZnJvbSBNaWM=\n",
+    b"SGVsbG8gZnJvbSBNaWNy\n",
+    b"SGVsbG8gZnJvbSBNaWNybw==\n",
+    b"SGVsbG8gZnJvbSBNaWNyb1A=\n",
+    b"SGVsbG8gZnJvbSBNaWNyb1B5\n",
+    b"SGVsbG8gZnJvbSBNaWNyb1B5dA==\n",
+    b"SGVsbG8gZnJvbSBNaWNyb1B5dGg=\n",
+    b"SGVsbG8gZnJvbSBNaWNyb1B5dGhv\n",
+    b"SGVsbG8gZnJvbSBNaWNyb1B5dGhvbg==\n",
+    b"SGVsbG8gZnJvbSBNaWNyb1B5dGhvbiE=\n",
+    b"SGVsbG8gZnJvbSBNaWNyb1B5dGhvbiEK\n",
+    b"SGVsbG8gZnJvbSBNaWNyb1B5dGhvbiEK/w==\n",
+    b"SGVsbG8gZnJvbSBNaWNyb1B5dGhvbiEK/4A=\n",
+    b"SGVsbG8gZnJvbSBNaWNyb1B5dGhvbiEK/4Cg\n",
+    b"SGVsbG8gZnJvbSBNaWNyb1B5dGhvbiEK/4Cgzw==\n",
+)
+
+for i in range(len(s) + 1):
+    substr = s[:i]
+    enc = b2a_base64(substr)
+    assert enc == b64[i]
+    dec = a2b_base64(enc)
+    assert dec == substr
 
 start = time.time()
 for x in range(100000):
